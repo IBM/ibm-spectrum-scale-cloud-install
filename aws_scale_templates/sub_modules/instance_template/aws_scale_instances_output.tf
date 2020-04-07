df25d0f3960fe9a9f380087af00267b2d2f19b5d
Sasikanth Eda <sasikeda@users.noreply.github.com>
2020-04-07 13:51:43 +0530

    Extend terraform to call ansible_scale_playbook (#27)
    Extend terraform to call ansible_scale_playbook

--- aws_scale_templates/sub_modules/instance_template/aws_scale_instances_output.tf
+++ aws_scale_templates/sub_modules/instance_template/aws_scale_instances_output.tf
@@ -11,10 +11,7 @@ output "compute_instance_ip_by_id" {
 }
 
 output "compute_instance_desc_map" {
-  value = {
-    for instance in module.desc_compute_instance.instance_ips_with_1_datadisks :
-    instance => slice(var.ebs_volume_device_names, 0, 1)
-  }
+  value = local.compute_instance_desc_map
 }
 
 output "storage_instance_ids_with_0_datadisks" {
@@ -218,113 +215,65 @@ output "storage_instance_ip_by_id_with_15_datadisks" {
 output "instance_ips_with_0_datadisks_ebs_device_names" {
   # This output has no significance.
   # Keeping it support ephemeral disks.
-  value = tonumber(var.ebs_volumes_per_instance) == 0 ? {
-    for instance in module.storage_instances.instance_ips_with_0_datadisks :
-    instance => slice(var.ebs_volume_device_names, 0, 0)
-  } : null
+  value = local.instance_ips_with_0_datadisks_ebs_device_names
 }
 
 output "instance_ips_with_1_datadisks_ebs_device_names" {
-  value = tonumber(var.ebs_volumes_per_instance) == 1 ? {
-    for instance in module.storage_instances.instance_ips_with_1_datadisks :
-    instance => slice(var.ebs_volume_device_names, 0, 1)
-  } : null
+  value = local.instance_ips_with_1_datadisks_ebs_device_names
 }
 
 output "instance_ips_with_2_datadisks_ebs_device_names" {
-  value = tonumber(var.ebs_volumes_per_instance) == 2 ? {
-    for instance in module.storage_instances.instance_ips_with_2_datadisks :
-    instance => slice(var.ebs_volume_device_names, 0, 2)
-  } : null
+  value = local.instance_ips_with_2_datadisks_ebs_device_names
 }
 
 output "instance_ips_with_3_datadisks_ebs_device_names" {
-  value = tonumber(var.ebs_volumes_per_instance) == 3 ? {
-    for instance in module.storage_instances.instance_ips_with_3_datadisks :
-    instance => slice(var.ebs_volume_device_names, 0, 3)
-  } : null
+  value = local.instance_ips_with_3_datadisks_ebs_device_names
 }
 
 output "instance_ips_with_4_datadisks_ebs_device_names" {
-  value = tonumber(var.ebs_volumes_per_instance) == 4 ? {
-    for instance in module.storage_instances.instance_ips_with_4_datadisks :
-    instance => slice(var.ebs_volume_device_names, 0, 4)
-  } : null
+  value = local.instance_ips_with_4_datadisks_ebs_device_names
 }
 
 output "instance_ips_with_5_datadisks_ebs_device_names" {
-  value = tonumber(var.ebs_volumes_per_instance) == 5 ? {
-    for instance in module.storage_instances.instance_ips_with_5_datadisks :
-    instance => slice(var.ebs_volume_device_names, 0, 5)
-  } : null
+  value = local.instance_ips_with_5_datadisks_ebs_device_names
 }
 
 output "instance_ips_with_6_datadisks_ebs_device_names" {
-  value = tonumber(var.ebs_volumes_per_instance) == 6 ? {
-    for instance in module.storage_instances.instance_ips_with_6_datadisks :
-    instance => slice(var.ebs_volume_device_names, 0, 6)
-  } : null
+  value = local.instance_ips_with_6_datadisks_ebs_device_names
 }
 
 output "instance_ips_with_7_datadisks_ebs_device_names" {
-  value = tonumber(var.ebs_volumes_per_instance) == 7 ? {
-    for instance in module.storage_instances.instance_ips_with_7_datadisks :
-    instance => slice(var.ebs_volume_device_names, 0, 7)
-  } : null
-}
-output "instance_ips_with_8_datadisks_ebs_device_names" {
-  value = tonumber(var.ebs_volumes_per_instance) == 8 ? {
-    for instance in module.storage_instances.instance_ips_with_8_datadisks :
-    instance => slice(var.ebs_volume_device_names, 0, 8)
-  } : null
+  value = local.instance_ips_with_7_datadisks_ebs_device_names
 }
 
+output "instance_ips_with_8_datadisks_ebs_device_names" {
+  value = local.instance_ips_with_8_datadisks_ebs_device_names
+}
 
 output "instance_ips_with_9_datadisks_ebs_device_names" {
-  value = tonumber(var.ebs_volumes_per_instance) == 9 ? {
-    for instance in module.storage_instances.instance_ips_with_9_datadisks :
-    instance => slice(var.ebs_volume_device_names, 0, 9)
-  } : null
+  value = local.instance_ips_with_9_datadisks_ebs_device_names
 }
 
 output "instance_ips_with_10_datadisks_ebs_device_names" {
-  value = tonumber(var.ebs_volumes_per_instance) == 10 ? {
-    for instance in module.storage_instances.instance_ips_with_10_datadisks :
-    instance => slice(var.ebs_volume_device_names, 0, 10)
-  } : null
+  value = local.instance_ips_with_10_datadisks_ebs_device_names
 }
 
 output "instance_ips_with_11_datadisks_ebs_device_names" {
-  value = tonumber(var.ebs_volumes_per_instance) == 11 ? {
-    for instance in module.storage_instances.instance_ips_with_11_datadisks :
-    instance => slice(var.ebs_volume_device_names, 0, 11)
-  } : null
+  value = local.instance_ips_with_11_datadisks_ebs_device_names
 }
 
 output "instance_ips_with_12_datadisks_ebs_device_names" {
-  value = tonumber(var.ebs_volumes_per_instance) == 12 ? {
-    for instance in module.storage_instances.instance_ips_with_12_datadisks :
-    instance => slice(var.ebs_volume_device_names, 0, 12)
-  } : null
+  value = local.instance_ips_with_12_datadisks_ebs_device_names
 }
 
 output "instance_ips_with_13_datadisks_ebs_device_names" {
-  value = tonumber(var.ebs_volumes_per_instance) == 13 ? {
-    for instance in module.storage_instances.instance_ips_with_13_datadisks :
-    instance => slice(var.ebs_volume_device_names, 0, 13)
-  } : null
+  value = local.instance_ips_with_13_datadisks_ebs_device_names
 }
 
 output "instance_ips_with_14_datadisks_ebs_device_names" {
-  value = tonumber(var.ebs_volumes_per_instance) == 14 ? {
-    for instance in module.storage_instances.instance_ips_with_14_datadisks :
-    instance => slice(var.ebs_volume_device_names, 0, 14)
-  } : null
+  value = local.instance_ips_with_14_datadisks_ebs_device_names
 }
 
 output "instance_ips_with_15_datadisks_ebs_device_names" {
-  value = tonumber(var.ebs_volumes_per_instance) == 15 ? {
-    for instance in module.storage_instances.instance_ips_with_15_datadisks :
-    instance => slice(var.ebs_volume_device_names, 0, 15)
-  } : null
+  value = local.instance_ips_with_15_datadisks_ebs_device_names
 }
