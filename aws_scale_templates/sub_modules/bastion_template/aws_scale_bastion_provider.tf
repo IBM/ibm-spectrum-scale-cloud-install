69fbd41b9dfc5d896ad1f7615910e551a88d599f
Sasikanth Eda <sasikanth.eda@in.ibm.com>
2021-02-18 16:27:01 +0530

    Remove deprecated provider version (support for v14)

--- aws_scale_templates/sub_modules/bastion_template/aws_scale_bastion_provider.tf
+++ aws_scale_templates/sub_modules/bastion_template/aws_scale_bastion_provider.tf
@@ -4,5 +4,4 @@ terraform {
 
 provider "aws" {
   region  = var.region
-  version = "~> 2.0"
 }
--- gcp_scale_templates/sub_modules/vpc_template/gcp_scale_vpc_provider.tf
+++ gcp_scale_templates/sub_modules/vpc_template/gcp_scale_vpc_provider.tf
@@ -3,7 +3,6 @@ terraform {
 }
 
 provider "google" {
-  version     = "~> 3.21"
   credentials = file(var.credentials_file_path)
   project     = var.gcp_project_id
   region      = var.region
