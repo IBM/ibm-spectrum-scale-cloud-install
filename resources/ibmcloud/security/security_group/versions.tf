a2feefd5cec548404c29c0cf7bea92efce09af74
Sasikanth Eda <sasikanth.eda@in.ibm.com>
2021-03-22 14:31:10 +0530

    Update ibmcloud provider to support terraform v0.13

---
+++ resources/ibmcloud/security/security_group/versions.tf
@@ -0,0 +1,14 @@
+terraform {
+  required_providers {
+    ibm = {
+      source = "IBM-Cloud/ibm"
+      version = "1.21.2"
+    }
+  }
+}
+
+provider "ibm" {
+  region           = var.region
+  ibmcloud_api_key = var.ibmcloud_api_key
+  generation       = var.compute_generation
+}
