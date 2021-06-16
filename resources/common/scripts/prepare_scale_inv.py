bc296f0b0b2194c12fab516297a46dc5ffdc5a48
Sasikanth Eda <sasikanth.eda@in.ibm.com>
2021-06-16 11:19:17 +0530

    Update inventory generation script

--- resources/common/scripts/prepare_scale_inv.py
+++ resources/common/scripts/prepare_scale_inv.py
@@ -19,11 +19,12 @@ limitations under the License.
 import argparse
 import configparser
 import json
-import os
 import pathlib
+import yaml
 
 
 def create_directory(target_directory):
+    """ Create specified directory """
     pathlib.Path(target_directory).mkdir(parents=True, exist_ok=True)
 
 
@@ -35,12 +36,14 @@ def read_json_file(json_path):
 
 
 def write_to_file(filepath, filecontent):
+    """ Write to specified file """
     with open(filepath, "w") as file_handler:
         file_handler.write(filecontent)
 
 
 def prepare_ansible_playbook(hosts_config, cluster_config):
-    playbook_content = """---
+    """ Write to playbook """
+    content = """---
 # Install and config Spectrum Scale on nodes
 - hosts: {hosts_config}
   any_errors_fatal: true
@@ -53,23 +56,23 @@ def prepare_ansible_playbook(hosts_config, cluster_config):
      - zimon/cluster
      - zimon/postcheck
 """.format(hosts_config=hosts_config, cluster_config=cluster_config)
-    return playbook_content
+    return content
 
 
-def initialize_cluster_details(scale_version, cluster_name, gui_username,
-                               gui_password, scale_profile_path,
+def initialize_cluster_details(scale_version, cluster_name, username,
+                               password, scale_profile_path,
                                scale_replica_config):
     """ Initialize cluster details.
     :args: scale_version (string), cluster_name (string),
-           gui_username (string), gui_password (string), scale_profile_path (string),
+           username (string), password (string), scale_profile_path (string),
            scale_replica_config (bool)
     """
     cluster_details = {}
     cluster_details['scale_version'] = scale_version
     cluster_details['scale_cluster_name'] = cluster_name
     cluster_details['scale_service_gui_start'] = "True"
-    cluster_details['scale_gui_admin_user'] = gui_username
-    cluster_details['scale_gui_admin_password'] = gui_password
+    cluster_details['scale_gui_admin_user'] = username
+    cluster_details['scale_gui_admin_password'] = password
     cluster_details['scale_gui_admin_role'] = "Administrator"
     cluster_details['ephemeral_port_range'] = "60000-61000"
     cluster_details['scale_sync_replication_config'] = scale_replica_config
@@ -80,25 +83,108 @@ def initialize_cluster_details(scale_version, cluster_name, gui_username,
     return cluster_details
 
 
-def initialize_node_details(ip_address, node_class, is_nsd_server=False,
-                            is_quorum_node=False, is_manager_node=False,
-                            is_collector_node=False, is_gui_server=False, is_admin_node=True):
+def get_host_format(node):
+    """ Return host entries """
+    host_format = f"{node['ip_addr']} scale_cluster_quorum={node['is_quorum']} scale_cluster_manager={node['is_manager']} scale_cluster_gui={node['is_gui']} scale_zimon_collector={node['is_collector']} is_nsd_server={node['is_nsd']} is_admin_node={node['is_admin']} ansible_user={node['user']} ansible_ssh_private_key_file={node['key_file']} scale_nodeclass={node['class']}"
+    return host_format
+
+
+def initialize_node_details(az_count, cluster_type, compute_private_ips,
+                            storage_private_ips, desc_private_ips, quorum_count,
+                            user, key_file):
     """ Initialize node details for cluster definition.
-    :args: json_data (json), fqdn (string), ip_address (string), node_class (string),
-           is_nsd_server (bool), is_quorum_node (bool),
-           is_manager_node (bool), is_collector_node (bool), is_gui_server (bool),
-           is_admin_node (bool)
+    :args: az_count (int), cluster_type (string), compute_private_ips (list),
+           storage_private_ips (list), desc_private_ips (list),
+           quorum_count (int), user, key_file
     """
-    CLUSTER_DEFINITION_JSON['node_details'].append({'ip_address': ip_address,
-                                                    'scale_cluster_quorum': is_quorum_node,
-                                                    'scale_cluster_manager': is_manager_node,
-                                                    'scale_cluster_gui': is_gui_server,
-                                                    'scale_zimon_collector': is_collector_node,
-                                                    'is_admin_node': is_admin_node,
-                                                    'is_nsd_server': is_nsd_server,
-                                                    'ansible_user': ansible_user,
-                                                    'ansible_ssh_private_key_file': ansible_ssh_private_key_file,
-                                                    'scale_nodeclass': node_class})
+    node_details = []
+    if cluster_type == 'compute':
+        for each_ip in compute_private_ips:
+            node = {}
+            if compute_private_ips.index(each_ip) == 0:
+                node = {'ip_addr': each_ip, 'is_quorum': True, 'is_manager': True,
+                        'is_gui': True, 'is_collector': True, 'is_nsd': False,
+                        'is_admin': True, 'user': user, 'key_file': key_file,
+                        'class': "computenodegrp"}
+            elif compute_private_ips.index(each_ip) == 1:
+                node = {'ip_addr': each_ip, 'is_quorum': True, 'is_manager': True,
+                        'is_gui': False, 'is_collector': True, 'is_nsd': False,
+                        'is_admin': False, 'user': user, 'key_file': key_file,
+                        'class': "computenodegrp"}
+            else:
+                node = {'ip_addr': each_ip, 'is_quorum': True, 'is_manager': False,
+                        'is_gui': False, 'is_collector': False, 'is_nsd': False,
+                        'is_admin': False, 'user': user, 'key_file': key_file,
+                        'class': "computenodegrp"}
+            node_details.append(get_host_format(node))
+    elif cluster_type == 'storage' and az_count == 1:
+        for each_ip in storage_private_ips:
+            node = {}
+            if storage_private_ips.index(each_ip) == 0:
+                node = {'ip_addr': each_ip, 'is_quorum': True, 'is_manager': True,
+                        'is_gui': True, 'is_collector': True, 'is_nsd': True,
+                        'is_admin': True, 'user': user, 'key_file': key_file,
+                        'class': "storagenodegrp"}
+            elif storage_private_ips.index(each_ip) == 1:
+                node = {'ip_addr': each_ip, 'is_quorum': True, 'is_manager': True,
+                        'is_gui': False, 'is_collector': True, 'is_nsd': True,
+                        'is_admin': False, 'user': user, 'key_file': key_file,
+                        'class': "storagenodegrp"}
+            else:
+                node = {'ip_addr': each_ip, 'is_quorum': True, 'is_manager': False,
+                        'is_gui': False, 'is_collector': True, 'is_nsd': True,
+                        'is_admin': False, 'user': user, 'key_file': key_file,
+                        'class': "storagenodegrp"}
+            node_details.append(get_host_format(node))
+    elif cluster_type == 'storage' and az_count > 1:
+        for each_ip in desc_private_ips:
+            node = {}
+            node = {'ip_addr': each_ip, 'is_quorum': True, 'is_manager': False,
+                    'is_gui': False, 'is_collector': False, 'is_nsd': True,
+                    'is_admin': False, 'user': user, 'key_file': key_file,
+                    'class': "computedescnodegrp"}
+            node_details.append(get_host_format(node))
+
+        if az_count > 1:
+            # Storage/NSD nodes to be quorum nodes (quorum_count - 2 as index starts from 0)
+            start_quorum_assign = quorum_count - 2
+        else:
+            # Storage/NSD nodes to be quorum nodes (quorum_count - 1 as index starts from 0)
+            start_quorum_assign = quorum_count - 1
+
+        for each_ip in storage_private_ips:
+            node = {}
+            if storage_private_ips.index(each_ip) <= (start_quorum_assign) and \
+                    storage_private_ips.index(each_ip) <= (manager_count - 1):
+                if storage_private_ips.index(each_ip) == 0:
+                    node = {'ip_addr': each_ip, 'is_quorum': True, 'is_manager': True,
+                            'is_gui': True, 'is_collector': True, 'is_nsd': True,
+                            'is_admin': True, 'user': user, 'key_file': key_file,
+                            'class': "storagenodegrp"}
+                elif storage_private_ips.index(each_ip) == 1:
+                    node = {'ip_addr': each_ip, 'is_quorum': True, 'is_manager': True,
+                            'is_gui': False, 'is_collector': True, 'is_nsd': True,
+                            'is_admin': True, 'user': user, 'key_file': key_file,
+                            'class': "storagenodegrp"}
+                else:
+                    node = {'ip_addr': each_ip, 'is_quorum': True, 'is_manager': True,
+                            'is_gui': False, 'is_collector': False, 'is_nsd': True,
+                            'is_admin': True, 'user': user, 'key_file': key_file,
+                            'class': "storagenodegrp"}
+            elif storage_private_ips.index(each_ip) <= (start_quorum_assign) and \
+                    storage_private_ips.index(each_ip) > (manager_count - 1):
+                node = {'ip_addr': each_ip, 'is_quorum': True, 'is_manager': False,
+                        'is_gui': False, 'is_collector': False, 'is_nsd': True,
+                        'is_admin': True, 'user': user, 'key_file': key_file,
+                        'class': "storagenodegrp"}
+            else:
+                node = {'ip_addr': each_ip, 'is_quorum': False, 'is_manager': False,
+                        'is_gui': False, 'is_collector': False, 'is_nsd': True,
+                        'is_admin': False, 'user': user, 'key_file': key_file,
+                        'class': "storagenodegrp"}
+
+            node_details.append(get_host_format(node))
+    return node_details
 
 
 def initialize_scale_config_details(node_class, param_key, param_value):
@@ -145,10 +231,7 @@ if __name__ == "__main__":
         gui_username = TF['storage_cluster_gui_username']
         gui_password = TF['storage_cluster_gui_password']
         profile_path = "%s/storagesncparams" % ARGUMENTS.install_infra_path
-        if len(TF['vpc_availability_zones']) > 1:
-            replica_config = True
-        else:
-            replica_config = False
+        replica_config = bool(len(TF['vpc_availability_zones']) > 1)
         scale_config = initialize_scale_config_details(
             "storagenodegrp", "pagepool", "1G")
     else:
@@ -156,10 +239,7 @@ if __name__ == "__main__":
         gui_username = TF['storage_cluster_gui_username']
         gui_password = TF['storage_cluster_gui_password']
         profile_path = "%s/scalesncparams" % ARGUMENTS.install_infra_path
-        if len(TF['vpc_availability_zones']) > 1:
-            replica_config = True
-        else:
-            replica_config = False
+        replica_config = bool(len(TF['vpc_availability_zones']) > 1)
         scale_config = initialize_scale_config_details(
             "storagenodegrp", "pagepool", "1G")
 
@@ -195,20 +275,41 @@ if __name__ == "__main__":
         print("Total quorum count: ", quorum_count)
 
     # Step-4: Create group_vars directory
-    os.chdir("%s/%s" % (ARGUMENTS.install_infra_path,
-             "ibm-spectrum-scale-install-infra"))
-    create_directory("group_vars")
+    create_directory("%s/%s/%s" % (ARGUMENTS.install_infra_path,
+                                   "ibm-spectrum-scale-install-infra",
+                                   "group_vars"))
 
     # Step-5: Create playbook
     playbook_content = prepare_ansible_playbook(
         "scale_nodes", "%s_cluster_config.yml" % cluster_type)
-    write_to_file("%s_cloud_playbook.yml" % cluster_type, playbook_content)
+    write_to_file("/%s/%s/%s_cloud_playbook.yml" % (ARGUMENTS.install_infra_path,
+                                                    "ibm-spectrum-scale-install-infra",
+                                                    cluster_type), playbook_content)
 
     if ARGUMENTS.verbose:
-        print("Content of ansible playbook: ", playbook_content)
+        print("Content of ansible playbook:\n", playbook_content)
 
-    # Step-6: Create hosts
-    config = configparser.ConfigParser()
+    # Step-6: Create group_vars
+    with open("%s/%s/%s/%s" % (ARGUMENTS.install_infra_path,
+                               "ibm-spectrum-scale-install-infra",
+                               "group_vars",
+                               "%s_cluster_config.yaml" % cluster_type), 'w') as groupvar:
+        yaml.dump(scale_config, groupvar, default_flow_style=False)
+    if ARGUMENTS.verbose:
+        print("group_vars content:\n%s" % yaml.dump(
+            scale_config, default_flow_style=False))
+
+    # Step-7: Create hosts
+    config = configparser.ConfigParser(allow_no_value=True)
+    node_details = initialize_node_details(len(TF['vpc_availability_zones']), cluster_type,
+                                           TF['compute_cluster_instance_private_ips'],
+                                           TF['storage_cluster_instance_private_ips'],
+                                           TF['storage_cluster_desc_instance_private_ips'],
+                                           quorum_count, "root", "key")
+    node_template = ""
+    for each_entry in node_details:
+        node_template = node_template + each_entry + "\n"
+
     config['all:vars'] = initialize_cluster_details(TF['scale_version'],
                                                     "%s.%s" % (
                                                         "spectrum-scale", cluster_type),
@@ -216,11 +317,22 @@ if __name__ == "__main__":
                                                     gui_password,
                                                     profile_path,
                                                     replica_config)
-    with open("%s_inventory.ini" % cluster_type, 'w') as configfile:
+    with open("%s/%s/%s_inventory.ini" % (ARGUMENTS.install_infra_path,
+                                          "ibm-spectrum-scale-install-infra",
+                                          cluster_type), 'w') as configfile:
+        configfile.write('[scale_nodes]' + "\n")
+        configfile.write(node_template)
         config.write(configfile)
 
     if ARGUMENTS.verbose:
-        config.read("%s_inventory.ini" % cluster_type)
+        config.read("%s/%s/%s_inventory.ini" % (ARGUMENTS.install_infra_path,
+                                                "ibm-spectrum-scale-install-infra",
+                                                cluster_type))
+        print("Content of %s/%s/%s_inventory.ini" % (ARGUMENTS.install_infra_path,
+                                                     "ibm-spectrum-scale-install-infra",
+                                                     cluster_type))
+        print('[scale_nodes]')
+        print(node_template)
+        print('[all:vars]')
         for each_key in config['all:vars']:
-            print("Content of %s: %s" %
-                  (each_key, config.get('all:vars', each_key)))
+            print("%s: %s" % (each_key, config.get('all:vars', each_key)))
