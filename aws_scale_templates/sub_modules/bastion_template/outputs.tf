b6c6113eba035c60aa13015e41e11cd50f35d0e6
Sasikanth Eda <sasikanth.eda@in.ibm.com>
2021-07-27 16:40:30 +0530

    Update with Bastion id and strictkeychecking
    Signed-off-by: Sasikanth Eda <sasikanth.eda@in.ibm.com>

--- aws_scale_templates/sub_modules/bastion_template/outputs.tf
+++ aws_scale_templates/sub_modules/bastion_template/outputs.tf
@@ -3,6 +3,11 @@ output "bastion_security_group_id" {
   description = "Bastion security group id."
 }
 
+output "bastion_instance_id" {
+  value       = module.bastion_autoscaling_group.asg_instance_id
+  description = "Bastion instance id."
+}
+
 output "bastion_instance_public_ip" {
   value       = module.bastion_autoscaling_group.asg_instance_public_ip
   description = "Bastion instance public ip addresses."
--- resources/common/scripts/prepare_remote_mount_inv.py
+++ resources/common/scripts/prepare_remote_mount_inv.py
@@ -186,7 +186,7 @@ if __name__ == "__main__":
         else:
             if STRG_TF['cloud_platform'] == 'AWS':
                 bastion_user = "ec2-user"
-            proxy_command = f"ssh -p 22 -W %h:%p {bastion_user}@{ARGUMENTS.bastion_ip} -i {ARGUMENTS.bastion_ssh_private_key}"
+            proxy_command = f"ssh -p 22 -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -W %h:%p {bastion_user}@{ARGUMENTS.bastion_ip} -i {ARGUMENTS.bastion_ssh_private_key}"
             each_entry = each_entry + " " + \
                 "ansible_ssh_common_args='-o ControlMaster=auto -o ControlPersist=30m -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -o ProxyCommand=\"" + proxy_command + "\"'"
             node_template = node_template + each_entry + "\n"
