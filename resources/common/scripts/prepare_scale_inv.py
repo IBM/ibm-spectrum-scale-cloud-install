6e0f582d02063adebaecde9b85bcafaf064853f2
Sasikanth Eda <sasikanth.eda@in.ibm.com>
2021-09-22 12:10:02 +0530

    Fix storage cluster gui in multi-az
    Signed-off-by: Sasikanth Eda <sasikanth.eda@in.ibm.com>

--- resources/common/scripts/prepare_scale_inv.py
+++ resources/common/scripts/prepare_scale_inv.py
@@ -283,6 +283,9 @@ def initialize_node_details(az_count, cls_type, compute_private_ips,
                             'is_gui': True, 'is_collector': True, 'is_nsd': True,
                             'is_admin': True, 'user': user, 'key_file': key_file,
                             'class': "storagenodegrp"}
+                    write_json_file({'storage_cluster_gui_ip_address': each_ip},
+                                    "%s/%s" % (str(pathlib.PurePath(ARGUMENTS.tf_inv_path).parent),
+                                               "storage_cluster_gui_details.json"))
                 elif storage_private_ips.index(each_ip) == 1:
                     node = {'ip_addr': each_ip, 'is_quorum': True, 'is_manager': True,
                             'is_gui': False, 'is_collector': True, 'is_nsd': True,
