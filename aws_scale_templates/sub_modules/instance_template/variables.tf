86d6596121c68ef709efd254cd785b2986c16019
Sasikanth Eda <sasikanth.eda@in.ibm.com>
2022-09-29 15:12:22 +0530

    Use nullable in Instance template

--- aws_scale_templates/sub_modules/instance_template/variables.tf
+++ aws_scale_templates/sub_modules/instance_template/variables.tf
@@ -1,10 +1,14 @@
 variable "vpc_region" {
   type        = string
+  nullable    = true
+  default     = null
   description = "The region where AWS operations will take place. Examples are us-east-1, us-west-2, etc."
 }
 
 variable "vpc_availability_zones" {
   type        = list(string)
+  nullable    = true
+  default     = null
   description = "A list of availability zones names or ids in the region."
 }
 
@@ -16,211 +20,258 @@ variable "resource_prefix" {
 
 variable "vpc_id" {
   type        = string
+  nullable    = true
+  default     = null
   description = "VPC id were to deploy the bastion."
 }
 
 variable "vpc_storage_cluster_private_subnets" {
   type        = list(string)
+  nullable    = true
+  default     = null
   description = "List of IDs of storage cluster private subnets."
 }
 
 variable "vpc_compute_cluster_private_subnets" {
   type        = list(string)
+  nullable    = true
+  default     = null
   description = "List of IDs of compute cluster private subnets."
 }
 
 variable "total_compute_cluster_instances" {
   type        = number
-  default     = 3
+  nullable    = true
+  default     = null
   description = "Number of EC2 instances to be launched for compute cluster."
 }
 
 variable "compute_cluster_key_pair" {
   type        = string
+  nullable    = true
+  default     = null
   description = "The key pair to use to launch the compute cluster host."
 }
 
 variable "compute_cluster_image_id" {
   type        = string
+  nullable    = true
+  default     = null
   description = "ID of AMI to use for provisioning the compute cluster instances."
 }
 
 variable "compute_cluster_instance_type" {
   type        = string
-  default     = "t2.medium"
+  nullable    = true
+  default     = null
   description = "Instance type to use for provisioning the compute cluster instances."
 }
 
 variable "using_rest_api_remote_mount" {
   type        = string
-  default     = true
+  nullable    = true
+  default     = null
   description = "If false, skips GUI initialization on compute cluster for remote mount configuration."
 }
 
 variable "compute_cluster_root_volume_type" {
   type        = string
-  default     = "gp2"
+  nullable    = true
+  default     = null
   description = "EBS volume types: standard, gp2, gp3, io1, io2 and sc1 or st1."
 }
 
 variable "compute_cluster_volume_tags" {
   type        = map(string)
-  default     = {}
+  nullable    = true
+  default     = null
   description = "Additional tags for the compute cluster volume(s)."
 }
 
 variable "compute_cluster_gui_username" {
   type        = string
+  nullable    = true
+  default     = null
   sensitive   = true
   description = "GUI user to perform system management and monitoring tasks on compute cluster."
 }
 
 variable "compute_cluster_gui_password" {
   type        = string
+  nullable    = true
+  default     = null
   sensitive   = true
   description = "Password for Compute cluster GUI."
 }
 
 variable "compute_cluster_tags" {
   type        = map(string)
-  default     = {}
+  nullable    = true
+  default     = null
   description = "Additional tags for the compute cluster."
 }
 
 variable "total_storage_cluster_instances" {
   type        = number
-  default     = 4
+  nullable    = true
+  default     = null
   description = "Number of EC2 instances to be launched for storage cluster."
 }
 
 variable "storage_cluster_key_pair" {
   type        = string
+  nullable    = true
+  default     = null
   description = "The key pair to use to launch the storage cluster host."
 }
 
 variable "storage_cluster_image_id" {
   type        = string
+  nullable    = true
+  default     = null
   description = "ID of AMI to use for provisioning the storage cluster instances."
 }
 
 variable "storage_cluster_instance_type" {
   type        = string
-  default     = "t2.medium"
+  nullable    = true
+  default     = null
   description = "Instance type to use for provisioning the storage cluster instances."
 }
 
 variable "storage_cluster_tags" {
   type        = map(string)
-  default     = {}
+  nullable    = true
+  default     = null
   description = "Additional tags for the storage cluster."
 }
 
 variable "storage_cluster_tiebreaker_instance_type" {
   type        = string
-  default     = "t2.medium"
+  nullable    = true
+  default     = null
   description = "Instance type to use for the tie breaker instance (will be provisioned only in Multi-AZ configuration)."
 }
 
 variable "storage_cluster_root_volume_type" {
   type        = string
-  default     = "gp2"
+  nullable    = true
+  default     = null
   description = "EBS volume types: standard, gp2, gp3, io1, io2 and sc1 or st1."
 }
 
 variable "storage_cluster_volume_tags" {
   type        = map(string)
-  default     = {}
+  nullable    = true
+  default     = null
   description = "Additional tags for the storage cluster volume(s)."
 }
 
 variable "storage_cluster_gui_username" {
   type        = string
+  nullable    = true
+  default     = null
   sensitive   = true
   description = "GUI user to perform system management and monitoring tasks on storage cluster."
 }
 
 variable "storage_cluster_gui_password" {
   type        = string
+  nullable    = true
+  default     = null
   sensitive   = true
   description = "Password for Storage cluster GUI"
 }
 
 variable "using_packer_image" {
   type        = bool
-  default     = false
+  nullable    = true
+  default     = null
   description = "If true, gpfs rpm copy step will be skipped during the configuration."
 }
 
 variable "using_direct_connection" {
   type        = bool
-  default     = false
+  nullable    = true
+  default     = null
   description = "If true, will skip the jump/bastion host configuration."
 }
 
 variable "ebs_block_devices_per_storage_instance" {
   type        = number
-  default     = 1
+  nullable    = true
+  default     = null
   description = "Additional EBS block devices to attach per storage cluster instance."
 }
 
 # Below parameters are only applicable if ebs_block_devices_per_storage_instance is set > 0
 variable "ebs_block_device_delete_on_termination" {
   type        = bool
-  default     = true
+  nullable    = true
+  default     = null
   description = "If true, all ebs volumes will be destroyed on instance termination."
 }
 
 variable "ebs_block_device_encrypted" {
   type        = bool
-  default     = false
+  nullable    = true
+  default     = null
   description = "Whether to enable volume encryption."
 }
 
 variable "ebs_block_device_iops" {
   type        = number
-  default     = 0
+  nullable    = true
+  default     = null
   description = "Amount of provisioned IOPS. Only valid for volume_type of io1, io2 or gp3."
 }
 
 variable "ebs_block_device_throughput" {
   type        = number
-  default     = 0
+  nullable    = true
+  default     = null
   description = "Throughput that the volume supports, in MiB/s. Only valid for volume_type of gp3."
 }
 
 variable "ebs_block_device_kms_key_id" {
   type        = string
+  nullable    = true
   default     = null
   description = "Amazon Resource Name (ARN) of the KMS Key to use when encrypting the volume."
 }
 
 variable "ebs_block_device_volume_size" {
   type        = number
-  default     = 500
+  nullable    = true
+  default     = null
   description = "Size of the volume in gibibytes (GiB)."
 }
 
 variable "ebs_block_device_volume_type" {
   type        = string
-  default     = "gp2"
+  nullable    = true
+  default     = null
   description = "EBS volume types: io1, io2, gp2, gp3, st1 and sc1."
 }
 
 variable "enable_nvme_block_device" {
   type        = bool
-  default     = false
+  nullable    = true
+  default     = null
   description = "Enable NVMe block devices (built on Nitro instances)."
 }
 
 variable "scale_ansible_repo_clone_path" {
   type        = string
-  default     = "/opt/IBM/ibm-spectrumscale-cloud-deploy"
+  nullable    = true
+  default     = null
   description = "Path to clone github.com/IBM/ibm-spectrum-scale-install-infra."
 }
 
 variable "spectrumscale_rpms_path" {
   type        = string
-  default     = "/opt/IBM/gpfs_cloud_rpms"
+  nullable    = true
+  default     = null
   description = "Path that contains IBM Spectrum Scale product cloud rpms."
 }
 
@@ -231,54 +282,63 @@ variable "operator_email" {
 
 variable "storage_cluster_filesystem_mountpoint" {
   type        = string
-  default     = "/gpfs/fs1"
+  nullable    = true
+  default     = null
   description = "Storage cluster (owningCluster) Filesystem mount point."
 }
 
 variable "compute_cluster_filesystem_mountpoint" {
   type        = string
-  default     = "/gpfs/fs1"
+  nullable    = true
+  default     = null
   description = "Compute cluster (accessingCluster) Filesystem mount point."
 }
 
 variable "filesystem_block_size" {
   type        = string
-  default     = "4M"
+  nullable    = true
+  default     = null
   description = "Filesystem block size."
 }
 
 variable "create_separate_namespaces" {
   type        = bool
-  default     = true
+  nullable    = true
+  default     = null
   description = "Flag to select if separate namespace needs to be created for compute instances."
 }
 
 variable "bastion_instance_public_ip" {
   type        = string
+  nullable    = true
   default     = null
   description = "Bastion instance public ip address."
 }
 
 variable "bastion_security_group_id" {
   type        = string
+  nullable    = true
   default     = null
   description = "Bastion security group id."
 }
 
 variable "bastion_instance_id" {
   type        = string
+  nullable    = true
   default     = null
   description = "Bastion instance id."
 }
 
 variable "bastion_ssh_private_key" {
   type        = string
+  nullable    = true
   default     = null
   description = "Bastion SSH private key path, which will be used to login to bastion host."
 }
 
 variable "create_scale_cluster" {
   type        = bool
-  default     = false
+  nullable    = true
+  default     = null
   description = "Flag to represent whether to create scale cluster or not."
 }
