fe84bd591aaf0ca20dd4c42fadeae900a495f839
Sasikanth Eda <sasikeda@users.noreply.github.com>
2022-10-12 14:53:08 +0530

    Update sns.tf

--- resources/aws/sns/sns.tf
+++ resources/aws/sns/sns.tf
@@ -2,19 +2,21 @@
     Creates new AWS SNS topic.
 */
 
+variable "turn_on" {}
 variable "vpc_region" {}
 variable "operator_email" {}
 variable "topic_name" {}
 
 #tfsec:ignore:AWS016
 resource "aws_sns_topic" "itself" {
+  count = var.turn_on
   name = var.topic_name
   provisioner "local-exec" {
     interpreter = ["/bin/bash", "-c"]
-    command     = "aws sns subscribe --topic-arn ${aws_sns_topic.itself.arn} --protocol email --notification-endpoint ${var.operator_email} --region ${var.vpc_region}"
+    command     = "aws sns subscribe --topic-arn ${aws_sns_topic.itself[0].arn} --protocol email --notification-endpoint ${var.operator_email} --region ${var.vpc_region}"
   }
 }
 
 output "topic_arn" {
-  value = aws_sns_topic.itself.arn
+  value = try(aws_sns_topic.itself[0].arn, null)
 }
