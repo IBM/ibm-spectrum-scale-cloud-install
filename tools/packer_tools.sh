b86610a79785c3715e7e21b72bcffe61719c3184
Sasikanth Eda <sasikeda@users.noreply.github.com>
2023-01-06 15:31:35 +0530

    Configurable bastion OS login user and gcp packer (#327)
    * Configurable bastion OS login user and gcp packer
    
    Signed-off-by: Sasikanth Eda <sasikanth.eda@in.ibm.com>
    
    * Add bastion OS login user to instance output
    
    Signed-off-by: Sasikanth Eda <sasikanth.eda@in.ibm.com>
    
    Signed-off-by: Sasikanth Eda <sasikanth.eda@in.ibm.com>

--- tools/packer_tools.sh
+++ tools/packer_tools.sh
@@ -5,3 +5,4 @@ root_dir=$(git rev-parse --show-toplevel)
 packer fmt $root_dir/packer_templates/aws
 packer fmt $root_dir/packer_templates/azure
 packer fmt $root_dir/packer_templates/ibmcloud
+packer fmt $root_dir/packer_templates/gcp
