8755db6d7c10e7b23dea2285fde11ca4cd9658c2
root <root@flays1.fyre.ibm.com>
2023-03-24 01:02:54 -0700

    updated vpc output vars
    Signed-off-by: root <root@flays1.fyre.ibm.com>

--- gcp_scale_templates/sub_modules/vpc_template/outputs.tf
+++ gcp_scale_templates/sub_modules/vpc_template/outputs.tf
@@ -8,27 +8,32 @@ output "vpc_id" {
   description = "The ID of the VPC."
 }
 
+output "cluster_type" {
+  value       = local.cluster_type
+  description = "Cluster type (Ex: storage, compute, combined)"
+}
+
 output "vpc_public_subnets" {
   value       = module.public_subnet.subnet_id
   description = "List of IDs of public subnets."
 }
 
 output "vpc_storage_cluster_private_subnets" {
-  value       = module.storage_private_subnet.subnet_id
+  value       = (local.cluster_type == "storage" || local.cluster_type == "combined") ? module.storage_private_subnet.subnet_id : null
   description = "List of IDs of storage cluster private subnets."
 }
 
 output "vpc_compute_cluster_private_subnets" {
-  value       = module.compute_private_subnet.subnet_id
+  value       = (local.cluster_type == "compute" || local.cluster_type == "combined") ? module.compute_private_subnet.subnet_id : null
   description = "List of IDs of compute cluster private subnets."
 }
 
-output "vpc_compute_cluster_nat" {
-  value       = module.compute_cloud_nat.cloud_nat_id
-  description = "List of IDs of compute cluster nat."
+output "vpc_compute_nat_gateways" {
+  value       = (local.cluster_type == "compute" || local.cluster_type == "combined") ? module.compute_cloud_nat.cloud_nat_id : null
+  description = "List of IDs of compute cluster nat gateway."
 }
 
-output "vpc_storage_cluster_nat" {
-  value       = module.storage_cloud_nat.cloud_nat_id
-  description = "List of IDs of storage cluster nat."
+output "vpc_storage_nat_gateways" {
+  value       = (local.cluster_type == "storage" || local.cluster_type == "combined") ? module.storage_cloud_nat.cloud_nat_id : null
+  description = "List of IDs of storage cluster nat gateway."
 }
