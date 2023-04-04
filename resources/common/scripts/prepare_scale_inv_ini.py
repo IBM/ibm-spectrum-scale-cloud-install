d23c9ffedd7ae76ce32ddd6555e8571a7e5693d4
jayeshh123 <127783319+jayeshh123@users.noreply.github.com>
2023-04-04 17:53:00 +0530

    Removed scale_install_prereqs_packages
    Removed scale_install_prereqs_packages and increased retries to 60.

--- resources/common/scripts/prepare_scale_inv_ini.py
+++ resources/common/scripts/prepare_scale_inv_ini.py
@@ -99,7 +99,7 @@ def prepare_ansible_playbook(hosts_config, cluster_config, cluster_key_file):
     when: scale_jump_host | bool
     register: result
     until: result.stdout.find("PASSWDLESS_SSH_ENABLED") != -1
-    retries: 30
+    retries: 60
     delay: 10
 
   - name: check | Check passwordless SSH on all scale inventory hosts without Bastion
@@ -109,7 +109,7 @@ def prepare_ansible_playbook(hosts_config, cluster_config, cluster_key_file):
     when: not scale_jump_host
     register: result
     until: result.stdout.find("PASSWDLESS_SSH_ENABLED") != -1
-    retries: 30
+    retries: 60
     delay: 10
 # Validate Scale packages existence to skip node role
 - name: Check if Scale packages already installed on node
@@ -237,7 +237,7 @@ def initialize_cluster_details(scale_version, cluster_name, username,
 
 def get_host_format(node):
     """ Return host entries """
-    host_format = f"{node['ip_addr']} scale_cluster_quorum={node['is_quorum']} scale_cluster_manager={node['is_manager']} scale_cluster_gui={node['is_gui']} scale_zimon_collector={node['is_collector']} is_nsd_server={node['is_nsd']} is_admin_node={node['is_admin']} ansible_user={node['user']} ansible_ssh_private_key_file={node['key_file']} ansible_python_interpreter=auto scale_install_prereqs_packages=True scale_nodeclass={node['class']}"
+    host_format = f"{node['ip_addr']} scale_cluster_quorum={node['is_quorum']} scale_cluster_manager={node['is_manager']} scale_cluster_gui={node['is_gui']} scale_zimon_collector={node['is_collector']} is_nsd_server={node['is_nsd']} is_admin_node={node['is_admin']} ansible_user={node['user']} ansible_ssh_private_key_file={node['key_file']} ansible_python_interpreter=auto scale_nodeclass={node['class']}"
     return host_format
 
 
