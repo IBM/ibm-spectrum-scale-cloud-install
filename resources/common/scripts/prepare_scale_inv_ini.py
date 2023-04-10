b4083fd868b14bd3f240c3698b5deb34a777220e
Sagar BK <Sagar.BK1@ibm.com>
2023-04-10 12:01:11 +0530

    Formated the line structure
    Signed-off-by: Sagar BK <Sagar.BK1@ibm.com>

--- resources/common/scripts/prepare_scale_inv_ini.py
+++ resources/common/scripts/prepare_scale_inv_ini.py
@@ -227,7 +227,6 @@ def get_host_format(node):
     host_format = f"{node['ip_addr']} scale_cluster_quorum={node['is_quorum']} scale_cluster_manager={node['is_manager']} scale_cluster_gui={node['is_gui']} scale_zimon_collector={node['is_collector']} is_nsd_server={node['is_nsd']} is_admin_node={node['is_admin']} ansible_user={node['user']} ansible_ssh_private_key_file={node['key_file']} ansible_python_interpreter=/usr/bin/python3 scale_nodeclass={node['class']}"
     return host_format
 
-
 def initialize_node_details(az_count, cls_type, compute_private_ips,
                             storage_private_ips, desc_private_ips, quorum_count,
                             user, key_file):
