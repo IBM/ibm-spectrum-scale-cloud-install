893da018e31a060363907325cb791714b46de6cf
Jayesh Kumar <Jayesh.Kumar@ibm.com>
2023-08-11 15:28:57 +0530

    fixed error11

--- resources/ibmcloud/compute/vsi_0_vol/vsi_0_vol.tf
+++ resources/ibmcloud/compute/vsi_0_vol/vsi_0_vol.tf
@@ -284,11 +284,11 @@ output "details" {
 }
 
 output "secondary_interface_name_id_map" {
-  value = try({for instance_details in ibm_is_instance.itself : one(instance_details.network_interfaces[*].name) => instance_details.id } , {})
+  value = try({for instance_details in ibm_is_instance.itself : "${one(instance_details.network_interfaces[*].name)}.${var.dns_domain}" => instance_details.id } , {})
   depends_on = [ibm_dns_resource_record.a_itself, ibm_dns_resource_record.ptr_itself]
 }
 
 output "secondary_interface_name_ip_map" {
-  value = try({for instance_details in ibm_is_instance.itself : "${one(instance_details.network_interfaces[*].name)}.${var.dns_domain}" => one(one(instance_details.network_interfaces[*].primary_ip[*].address)) }, {})
+  value = try({for instance_details in ibm_is_instance.itself : one(instance_details.network_interfaces[*].name) => one(one(instance_details.network_interfaces[*].primary_ip[*].address)) }, {})
   depends_on = [ibm_dns_resource_record.a_itself, ibm_dns_resource_record.ptr_itself]
 }
