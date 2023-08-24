be0b696e46c647179cea8e80a0e601bee8675dc0
Jhony-Louies1 <jhony.louies1@ibm.com>
2023-08-24 19:00:10 +0530

    [IBMCloud] Scale Encryption with GKLM
    Signed-off-by: Jhony-Louies1 <jhony.louies1@ibm.com>

--- resources/common/encryption_configuration/encryption_configuration.tf
+++ resources/common/encryption_configuration/encryption_configuration.tf
@@ -5,6 +5,7 @@
 variable "turn_on" {}
 variable "clone_path" {}
 variable "clone_complete" {}
+variable "create_scale_cluster" {}
 variable "meta_private_key" {}
 variable "scale_cluster_clustername" {}
 variable "scale_encryption_servers" {}
@@ -37,7 +38,7 @@ resource "local_sensitive_file" "write_meta_private_key" {
 }
 
 resource "null_resource" "perform_encryption_prepare" {
-  count = (tobool(var.turn_on) == true && tobool(var.clone_complete) == true) ? 1 : 0
+  count = (tobool(var.turn_on) == true && tobool(var.clone_complete) == true && tobool(var.create_scale_cluster) == true) ? 1 : 0
   provisioner "local-exec" {
     interpreter = ["/bin/bash", "-c"]
     command     = "/usr/local/bin/ansible-playbook -f 32 ${local.encryption_gklm_playbook} -e scale_cluster_clustername=${var.scale_cluster_clustername} -e ansible_ssh_private_key_file=${local.gklm_private_key} -e scale_encryption_admin_default_password=${var.scale_encryption_admin_default_password} -e scale_encryption_admin_password=${var.scale_encryption_admin_password} -e scale_encryption_admin_user=${var.scale_encryption_admin_username} -e scale_encryption_servers_list=${local.scale_encryption_servers}"
@@ -45,7 +46,7 @@ resource "null_resource" "perform_encryption_prepare" {
 }
 
 resource "null_resource" "perform_encryption_storage" {
-  count = (tobool(var.turn_on) == true && tobool(var.storage_cluster_encryption) == true && tobool(var.storage_cluster_create_complete) == true && tobool(var.remote_mount_create_complete) == true) ? 1 : 0
+  count = (tobool(var.turn_on) == true && tobool(var.storage_cluster_encryption) == true && tobool(var.storage_cluster_create_complete) == true && tobool(var.remote_mount_create_complete) == true && tobool(var.create_scale_cluster) == true) ? 1 : 0
   provisioner "local-exec" {
     interpreter = ["/bin/bash", "-c"]
     command     = "/usr/local/bin/ansible-playbook -f 32 -i ${local.storage_inventory_path} ${local.encryption_cluster_playbook}"
@@ -53,7 +54,7 @@ resource "null_resource" "perform_encryption_storage" {
 }
 
 resource "null_resource" "perform_encryption_compute" {
-  count = (tobool(var.turn_on) == true && tobool(var.compute_cluster_encryption) == true && tobool(var.compute_cluster_create_complete) == true && tobool(var.remote_mount_create_complete) == true) ? 1 : 0
+  count = (tobool(var.turn_on) == true && tobool(var.compute_cluster_encryption) == true && tobool(var.compute_cluster_create_complete) == true && tobool(var.remote_mount_create_complete) == true && tobool(var.create_scale_cluster) == true) ? 1 : 0
   provisioner "local-exec" {
     interpreter = ["/bin/bash", "-c"]
     command     = "/usr/local/bin/ansible-playbook -f 32 -i ${local.compute_inventory_path} ${local.encryption_cluster_playbook}"
@@ -61,7 +62,7 @@ resource "null_resource" "perform_encryption_compute" {
 }
 
 resource "null_resource" "perform_encryption_combined" {
-  count = (tobool(var.turn_on) == true && tobool(var.combined_cluster_encryption) == true && tobool(var.combined_cluster_create_complete) == true) ? 1 : 0
+  count = (tobool(var.turn_on) == true && tobool(var.combined_cluster_encryption) == true && tobool(var.combined_cluster_create_complete) == true && tobool(var.create_scale_cluster) == true) ? 1 : 0
   provisioner "local-exec" {
     interpreter = ["/bin/bash", "-c"]
     command     = "/usr/local/bin/ansible-playbook -f 32 -i ${local.combined_inventory_path} ${local.encryption_cluster_playbook}"
