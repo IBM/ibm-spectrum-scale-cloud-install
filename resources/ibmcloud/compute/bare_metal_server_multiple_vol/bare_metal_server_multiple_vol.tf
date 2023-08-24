a881ff67394e9d552b0136329c8db8338818ebde
Jayesh Kumar <Jayesh.Kumar@ibm.com>
2023-08-24 13:24:58 +0530

    Updated name for ip and dns map
    Signed-off-by: Jayesh Kumar <Jayesh.Kumar@ibm.com>

--- resources/ibmcloud/compute/bare_metal_server_multiple_vol/bare_metal_server_multiple_vol.tf
+++ resources/ibmcloud/compute/bare_metal_server_multiple_vol/bare_metal_server_multiple_vol.tf
@@ -163,7 +163,7 @@ output "instance_ips_with_vol_mapping" {
 }
 
 output "instance_private_dns_ip_map" {
-  value = try({ for instance_details in ibm_is_bare_metal_server.itself : instance_details.primary_network_interface[0]["primary_ip"][0]["address"] => instance_details.private_dns }, {})
+  value = try({ for instance_details in ibm_is_bare_metal_server.itself : instance_details.name => instance_details.private_dns }, {})
 }
 
 output "storage_cluster_instance_name_id_map" {
