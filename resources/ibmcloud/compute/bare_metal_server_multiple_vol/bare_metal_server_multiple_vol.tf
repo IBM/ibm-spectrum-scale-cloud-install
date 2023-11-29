872ecaabd88a8f6cdad8669800dbcd2922c9e975
Jhony-Louies1 <jhony.louies1@ibm.com>
2023-11-29 18:53:29 +0530

    [IBMCloud] Bug Fix and description update
    Signed-off-by: Jhony-Louies1 <jhony.louies1@ibm.com>

--- resources/ibmcloud/compute/bare_metal_server_multiple_vol/bare_metal_server_multiple_vol.tf
+++ resources/ibmcloud/compute/bare_metal_server_multiple_vol/bare_metal_server_multiple_vol.tf
@@ -57,7 +57,7 @@ echo "StrictHostKeyChecking no" >> ~/.ssh/config
 echo "DOMAIN=\"${var.dns_domain}\"" >> "/etc/sysconfig/network-scripts/ifcfg-eth0"
 echo "MTU=9000" >> "/etc/sysconfig/network-scripts/ifcfg-eth0"
 sed -i -e "s#QUEUE_COUNT=3#QUEUE_COUNT=\`ethtool -l \$iface | echo \$(awk '\$1 ~ /Combined:/ {print \$2;exit}')\`#g" /var/lib/cloud/scripts/per-boot/iface-config
-ethtool -L ens1 combined 16
+ethtool -L eth0 combined 16
 chage -I -1 -m 0 -M 99999 -E -1 -W 14 vpcuser
 systemctl restart NetworkManager
 systemctl stop firewalld
