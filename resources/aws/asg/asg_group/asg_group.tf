b9c50b639cf3e6327ed5be6dc138a971db67cdfa
Sasikanth Eda <sasikeda@users.noreply.github.com>
2023-11-30 11:58:55 +0530

    Fix the asg_group id

--- resources/aws/asg/asg_group/asg_group.tf
+++ resources/aws/asg/asg_group/asg_group.tf
@@ -44,6 +44,6 @@ resource "aws_autoscaling_group" "itself" {
   }
 }
 
-output "asg_arn" {
-  value = aws_autoscaling_group.itself[*].arn
+output "asg_id" {
+  value = aws_autoscaling_group.itself[*].id
 }
