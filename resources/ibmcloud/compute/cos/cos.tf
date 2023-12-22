66cf9a4b9fdc84e3e5c1a93549591002902a2dc8
Jayesh Kumar <Jayesh.Kumar@ibm.com>
2023-12-22 19:42:31 +0530

    afm17
    Signed-off-by: Jayesh Kumar <Jayesh.Kumar@ibm.com>

--- resources/ibmcloud/compute/cos/cos.tf
+++ resources/ibmcloud/compute/cos/cos.tf
@@ -6,27 +6,11 @@ terraform {
   }
 }
 
-
-/**
-#################################################################################################################
-*                           Resources Section for the COS Bucket Module.
-#################################################################################################################
-*/
-
 variable "prefix" {}
 variable "resource_group_id" {}
 variable "cos_bucket_plan" {}
 variable "region_location" {}
 variable "storage_class" {}
-# variable "bucket_location" {}
-# variable "obj_key" {}
-# variable "obj_content" {}
-
-/**
-* COS Instance
-* Element : resource_instance
-* This resource will be used to create a resource instance.
-**/
 
 resource "ibm_resource_instance" "cos_instance" {
   name              = "${var.prefix}instance"
@@ -36,11 +20,6 @@ resource "ibm_resource_instance" "cos_instance" {
   service           = "cloud-object-storage"
 }
 
-/**
-* COS Bucket
-* Element : cos_bucket
-* This resource will be used to create a COS Bucket.
-**/
 resource "ibm_cos_bucket" "cos_bucket" {
   bucket_name          = "${var.prefix}bucket"
   resource_instance_id = ibm_resource_instance.cos_instance.id
@@ -48,16 +27,3 @@ resource "ibm_cos_bucket" "cos_bucket" {
   storage_class        = var.storage_class
   depends_on           = [ibm_resource_instance.cos_instance]
 }
-
-/**
-* COS Bucket Object
-* Element : cos_object
-* This resource will be used to create a COS Bucket Object.
-**/
-# resource "ibm_cos_bucket_object" "cos_object" {
-#   bucket_crn      = ibm_cos_bucket.cos_bucket.crn
-#   bucket_location = var.bucket_location
-#   key             = var.obj_key
-#   content         = var.obj_content
-#   depends_on      = [ibm_cos_bucket.cos_bucket]
-# }
