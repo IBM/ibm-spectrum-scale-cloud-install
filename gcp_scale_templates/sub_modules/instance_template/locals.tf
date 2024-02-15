dae59343488fdd7a7961c0834026e0a3dc37472a
Sasikanth Eda <sasikanth.eda@in.ibm.com>
2024-02-15 14:21:54 +0530

    Fix the disk loop iterator for nitro
    Signed-off-by: Sasikanth Eda <sasikanth.eda@in.ibm.com>

--- gcp_scale_templates/sub_modules/instance_template/locals.tf
+++ gcp_scale_templates/sub_modules/instance_template/locals.tf
@@ -4,15 +4,10 @@
 */
 
 locals {
-  cluster_type = (
-    (var.vpc_storage_cluster_private_subnets != null && var.vpc_compute_cluster_private_subnets == null) ? "storage" :
-    (var.vpc_storage_cluster_private_subnets == null && var.vpc_compute_cluster_private_subnets != null) ? "compute" :
-    (var.vpc_storage_cluster_private_subnets != null && var.vpc_compute_cluster_private_subnets != null) ? "combined" : null
-  )
-  compute_or_combined  = ((local.cluster_type == "compute" || local.cluster_type == "combined") && var.total_compute_cluster_instances != null) ? true : false
-  storage_or_combined  = ((local.cluster_type == "storage" || local.cluster_type == "combined") && var.total_storage_cluster_instances != null) ? true : false
-  storage_and_protocol = ((local.cluster_type == "storage" || local.cluster_type == "combined") && var.total_protocol_instances != null) ? true : false
-  storage_and_gateway  = ((local.cluster_type == "storage" || local.cluster_type == "combined") && var.total_gateway_instances != null) ? true : false
+  compute_or_combined  = ((var.cluster_type == "Compute-only" || var.cluster_type == "Combined-compute-storage") && var.total_compute_cluster_instances > 0) ? true : false
+  storage_or_combined  = ((var.cluster_type == "Storage-only" || var.cluster_type == "Combined-compute-storage") && var.total_storage_cluster_instances > 0) ? true : false
+  storage_and_protocol = ((var.cluster_type == "Storage-only" || var.cluster_type == "Combined-compute-storage") && var.total_protocol_instances > 0) ? true : false
+  storage_and_gateway  = ((var.cluster_type == "Storage-only" || var.cluster_type == "Combined-compute-storage") && var.total_gateway_instances > 0) ? true : false
 
   tcp_port_scale_cluster    = ["22", "1191", "60000-61000", "47080", "4444", "4739", "9080", "9081", "80", "443"]
   udp_port_scale_cluster    = ["47443", "4739"]
--- gcp_scale_templates/sub_modules/instance_template/variables.tf
+++ gcp_scale_templates/sub_modules/instance_template/variables.tf
@@ -1,79 +1,49 @@
-variable "project_id" {
-  type        = string
-  nullable    = false
-  description = "GCP project ID to manage resources."
+variable "airgap" {
+  type        = bool
+  nullable    = true
+  description = "If true, instance iam profile, git utils which need internet access will be skipped."
 }
 
-variable "vpc_region" {
-  type        = string
-  nullable    = false
-  description = "GCP region where the resources will be created."
-}
-
-variable "vpc_ref" {
-  type        = string
-  nullable    = false
-  description = "VPC id were to deploy the bastion."
-}
-
-variable "vpc_availability_zones" {
-  type        = list(string)
-  nullable    = false
-  description = "A list of availability zones names or ids in the region."
-}
-
-variable "resource_prefix" {
-  type        = string
-  nullable    = false
-  description = "GCP stack name, will be used for tagging resources."
-}
-
-variable "instances_ssh_user_name" {
-  type        = string
-  nullable    = false
-  description = "Compute/Storage VM login username."
-}
-
-variable "compute_cluster_public_key_path" {
+variable "bastion_instance_public_ip" {
   type        = string
   nullable    = true
-  description = "SSH public key local path for compute instances."
+  description = "Bastion instance public ip address."
 }
 
-variable "storage_cluster_public_key_path" {
+variable "bastion_instance_ref" {
   type        = string
   nullable    = true
-  description = "SSH public key local path for storage instances."
+  description = "Bastion instance reference."
 }
 
-variable "credential_json_path" {
+variable "bastion_security_group_ref" {
+  type        = string
+  nullable    = true
+  description = "Bastion security group reference (id/self-link)."
+}
+
+variable "bastion_ssh_private_key" {
+  type        = string
+  nullable    = true
+  description = "Bastion SSH private key path, which will be used to login to bastion host."
+}
+
+variable "bastion_user" {
+  type        = string
+  nullable    = true
+  description = "Bastion login username."
+}
+
+variable "client_security_group_ref" {
+  type        = string
+  nullable    = true
+  description = "Client security group reference (id/self-link)."
+}
+
+variable "cluster_type" {
   type        = string
   nullable    = false
-  description = "The path of a GCP service account key file in JSON format."
-}
-
-variable "vpc_storage_cluster_private_subnets" {
-  type        = list(string)
-  nullable    = true
-  description = "List of IDs of storage cluster private subnets."
-}
-
-variable "vpc_compute_cluster_private_subnets" {
-  type        = list(string)
-  nullable    = true
-  description = "List of IDs of compute cluster private subnets."
-}
-
-variable "total_compute_cluster_instances" {
-  type        = number
-  nullable    = true
-  description = "Number of GCP instances to be launched for compute cluster."
-}
-
-variable "compute_cluster_instance_type" {
-  type        = string
-  nullable    = true
-  description = "Instance type to use for provisioning the compute cluster instances."
+  description = "Cluster type to provision. Examples: Storage-only, Compute-only, Combined-compute-storage."
 }
 
 variable "compute_boot_disk_size" {
@@ -88,23 +58,10 @@ variable "compute_boot_disk_type" {
   description = "GCE disk type (valid: pd-standard, pd-ssd)."
 }
 
-variable "compute_cluster_image_ref" {
+variable "compute_cluster_filesystem_mountpoint" {
   type        = string
   nullable    = true
-  description = "Image from which to initialize Spectrum Scale compute instances."
-}
-
-variable "using_rest_api_remote_mount" {
-  type        = string
-  nullable    = true
-  description = "If false, skips GUI initialization on compute cluster for remote mount configuration."
-}
-
-variable "compute_cluster_gui_username" {
-  type        = string
-  nullable    = true
-  sensitive   = true
-  description = "GUI user to perform system management and monitoring tasks on compute cluster."
+  description = "Compute cluster (accessingCluster) Filesystem mount point."
 }
 
 variable "compute_cluster_gui_password" {
@@ -114,10 +71,53 @@ variable "compute_cluster_gui_password" {
   description = "Password for Compute cluster GUI."
 }
 
-variable "total_storage_cluster_instances" {
-  type        = number
+variable "compute_cluster_gui_username" {
+  type        = string
   nullable    = true
-  description = "Number of instances to be launched for storage instances."
+  sensitive   = true
+  description = "GUI user to perform system management and monitoring tasks on compute cluster."
+}
+
+variable "compute_cluster_image_ref" {
+  type        = string
+  nullable    = true
+  description = "Image from which to initialize Spectrum Scale compute instances."
+}
+
+variable "compute_cluster_instance_type" {
+  type        = string
+  nullable    = true
+  description = "Instance type to use for provisioning the compute cluster instances."
+}
+
+variable "compute_cluster_public_key_path" {
+  type        = string
+  nullable    = true
+  description = "SSH public key local path for compute instances."
+}
+
+variable "create_clouddns" {
+  type        = bool
+  nullable    = true
+  description = "Indicates whether to create new cloud DNS zones or reuse existing DNS zones."
+}
+
+variable "create_remote_mount_cluster" {
+  type        = bool
+  nullable    = true
+  description = "Flag to select if separate compute and storage cluster needs to be created and proceed for remote mount filesystem setup."
+}
+
+variable "create_scale_cluster" {
+  type        = bool
+  nullable    = true
+  description = "Flag to represent whether to create scale cluster or not."
+}
+
+variable "credential_json_path" {
+  type        = string
+  nullable    = false
+  description = "The path of a GCP service account key file in JSON format."
 }
 
 variable "filesystem_parameters" {
@@ -138,10 +138,46 @@ variable "filesystem_parameters" {
   description = "Filesystem parameters in relationship with disk parameters."
 }
 
-variable "root_device_kms_key_ring_ref" {
+variable "gateway_instance_type" {
   type        = string
   nullable    = true
-  description = "GCP KMS Key ring reference to use when encrypting the volume."
+  description = "Instance type to use for provisioning the gateway instances."
+}
+
+variable "instances_ssh_user_name" {
+  type        = string
+  nullable    = false
+  description = "Compute/Storage VM login username."
+}
+
+variable "inventory_format" {
+  type        = string
+  nullable    = true
+  description = "Specify inventory format suited for ansible playbooks."
+}
+
+variable "physical_block_size_bytes" {
+  type        = number
+  nullable    = true
+  description = "Physical block size of the persistent disk, in bytes (valid: 4096, 16384)."
+}
+
+variable "project_id" {
+  type        = string
+  nullable    = false
+  description = "GCP project ID to manage resources."
+}
+
+variable "protocol_instance_type" {
+  type        = string
+  nullable    = true
+  description = "Instance type to use for provisioning the protocol instances."
+}
+
+variable "resource_prefix" {
+  type        = string
+  nullable    = false
+  description = "GCP stack name, will be used for tagging resources."
 }
 
 variable "root_device_kms_key_ref" {
@@ -150,16 +186,40 @@ variable "root_device_kms_key_ref" {
   description = "GCP KMS Key reference to use when encrypting the volume."
 }
 
+variable "root_device_kms_key_ring_ref" {
+  type        = string
+  nullable    = true
+  description = "GCP KMS Key ring reference to use when encrypting the volume."
+}
+
+variable "scale_ansible_repo_clone_path" {
+  type        = string
+  nullable    = true
+  description = "Path to clone github.com/IBM/ibm-spectrum-scale-install-infra."
+}
+
+variable "scopes" {
+  type        = list(string)
+  nullable    = true
+  description = "List of service scopes."
+}
+
 variable "scratch_devices_per_storage_instance" {
   type        = number
   nullable    = true
   description = "Number of scratch disks to be attached to each storage instance."
 }
 
-variable "storage_cluster_instance_type" {
+variable "service_email" {
   type        = string
   nullable    = true
-  description = "GCP instance machine type to create Spectrum Scale storage instances."
+  description = "GCP service account e-mail address."
+}
+
+variable "spectrumscale_rpms_path" {
+  type        = string
+  nullable    = true
+  description = "Path that contains IBM Spectrum Scale product cloud rpms."
 }
 
 variable "storage_boot_disk_size" {
@@ -174,106 +234,11 @@ variable "storage_boot_disk_type" {
   description = "GCE disk type (valid: pd-standard, pd-ssd)."
 }
 
-variable "storage_cluster_image_ref" {
+variable "storage_cluster_gui_password" {
   type        = string
   nullable    = true
-  description = "Image from which to initialize Spectrum Scale storage instances."
-}
-
-variable "physical_block_size_bytes" {
-  type        = number
-  nullable    = true
-  description = "Physical block size of the persistent disk, in bytes (valid: 4096, 16384)."
-}
-
-variable "service_email" {
-  type        = string
-  nullable    = true
-  description = "GCP service account e-mail address."
-}
-
-variable "scopes" {
-  type        = list(string)
-  nullable    = true
-  description = "List of service scopes."
-}
-
-variable "scale_ansible_repo_clone_path" {
-  type        = string
-  nullable    = true
-  description = "Path to clone github.com/IBM/ibm-spectrum-scale-install-infra."
-}
-
-variable "create_remote_mount_cluster" {
-  type        = bool
-  nullable    = true
-  description = "Flag to select if separate compute and storage cluster needs to be created and proceed for remote mount filesystem setup."
-}
-
-variable "spectrumscale_rpms_path" {
-  type        = string
-  nullable    = true
-  description = "Path that contains IBM Spectrum Scale product cloud rpms."
-}
-
-variable "compute_cluster_filesystem_mountpoint" {
-  type        = string
-  nullable    = true
-  description = "Compute cluster (accessingCluster) Filesystem mount point."
-}
-
-variable "bastion_user" {
-  type        = string
-  nullable    = true
-  description = "Bastion login username."
-}
-
-variable "bastion_instance_public_ip" {
-  type        = string
-  nullable    = true
-  description = "Bastion instance public ip address."
-}
-
-variable "bastion_instance_ref" {
-  type        = string
-  nullable    = true
-  description = "Bastion instance reference."
-}
-
-variable "create_scale_cluster" {
-  type        = bool
-  nullable    = true
-  description = "Flag to represent whether to create scale cluster or not."
-}
-
-variable "inventory_format" {
-  type        = string
-  nullable    = true
-  description = "Specify inventory format suited for ansible playbooks."
-}
-
-variable "using_packer_image" {
-  type        = bool
-  nullable    = true
-  description = "If true, gpfs rpm copy step will be skipped during the configuration."
-}
-
-variable "using_direct_connection" {
-  type        = bool
-  nullable    = true
-  description = "This flag is intended to enable ansible related communication between an on-premise virtual machine (VM) to cloud virtual private cloud (VPC) via a VPN or direct connection. This mode requires variable `client_ip_ranges`, as the on-premise client ip will be added to the allowed ingress list of scale (storage/compute) cluster security groups."
-}
-
-variable "using_cloud_connection" {
-  type        = bool
-  nullable    = true
-  description = "This flag is intended to enable ansible related communication between a cloud virtual machine (VM) to cloud existing virtual private cloud (VPC). This mode requires variable `client_security_group_ref` (make sure it is in the same vpc), as the cloud VM security group reference (id/self-link) will be added to the allowed ingress list of scale (storage/compute) cluster security groups."
-}
-
-variable "using_jumphost_connection" {
-  type        = bool
-  nullable    = true
-  description = "This flag is intended to enable ansible related communication between an on-premise virtual machine (VM) to cloud existing virtual private cloud (VPC). This mode requires variable `bastion_user`, `bastion_instance_public_ip`, `bastion_ssh_private_key`, as the jump host related security group reference (id/self-link) will be added to the allowed ingress list of scale (storage/compute) cluster security groups."
+  sensitive   = true
+  description = "Password for Storage cluster GUI"
 }
 
 variable "storage_cluster_gui_username" {
@@ -283,29 +248,46 @@ variable "storage_cluster_gui_username" {
   description = "GUI user to perform system management and monitoring tasks on storage cluster."
 }
 
-variable "storage_cluster_gui_password" {
+variable "storage_cluster_image_ref" {
   type        = string
   nullable    = true
-  sensitive   = true
-  description = "Password for Storage cluster GUI"
+  description = "Image from which to initialize Spectrum Scale storage instances."
 }
 
-variable "bastion_ssh_private_key" {
+variable "storage_cluster_instance_type" {
   type        = string
   nullable    = true
-  description = "Bastion SSH private key path, which will be used to login to bastion host."
+  description = "GCP instance machine type to create Spectrum Scale storage instances."
 }
 
-variable "bastion_security_group_ref" {
+variable "storage_cluster_public_key_path" {
   type        = string
   nullable    = true
-  description = "Bastion security group reference (id/self-link)."
+  description = "SSH public key local path for storage instances."
 }
 
-variable "client_security_group_ref" {
-  type        = string
+variable "total_compute_cluster_instances" {
+  type        = number
   nullable    = true
-  description = "Client security group reference (id/self-link)."
+  description = "Number of GCP instances to be launched for compute cluster."
+}
+
+variable "total_gateway_instances" {
+  type        = number
+  nullable    = true
+  description = "Number of EC2 instances to be launched for gateway nodes."
+}
+
+variable "total_protocol_instances" {
+  type        = number
+  nullable    = true
+  description = "Number of EC2 instances to be launched for protocol nodes."
+}
+
+variable "total_storage_cluster_instances" {
+  type        = number
+  nullable    = true
+  description = "Number of instances to be launched for storage instances."
 }
 
 variable "use_clouddns" {
@@ -314,35 +296,83 @@ variable "use_clouddns" {
   description = "Indicates whether to use cloud DNS or internal DNS."
 }
 
-variable "create_clouddns" {
+variable "using_cloud_connection" {
   type        = bool
   nullable    = true
-  description = "Indicates whether to create new cloud DNS zones or reuse existing DNS zones."
+  description = "This flag is intended to enable ansible related communication between a cloud virtual machine (VM) to cloud existing virtual private cloud (VPC). This mode requires variable `client_security_group_ref` (make sure it is in the same vpc), as the cloud VM security group reference (id/self-link) will be added to the allowed ingress list of scale (storage/compute) cluster security groups."
 }
 
-variable "vpc_compute_cluster_private_subnets_cidr_block" {
-  type        = string
+variable "using_direct_connection" {
+  type        = bool
   nullable    = true
-  description = "cidr_block of compute private subnet."
+  description = "This flag is intended to enable ansible related communication between an on-premise virtual machine (VM) to cloud virtual private cloud (VPC) via a VPN or direct connection. This mode requires variable `client_ip_ranges`, as the on-premise client ip will be added to the allowed ingress list of scale (storage/compute) cluster security groups."
 }
 
-variable "vpc_storage_cluster_private_subnets_cidr_block" {
-  type        = string
+variable "using_jumphost_connection" {
+  type        = bool
   nullable    = true
-  description = "cidr_block of storage private subnet."
+  description = "This flag is intended to enable ansible related communication between an on-premise virtual machine (VM) to cloud existing virtual private cloud (VPC). This mode requires variable `bastion_user`, `bastion_instance_public_ip`, `bastion_ssh_private_key`, as the jump host related security group reference (id/self-link) will be added to the allowed ingress list of scale (storage/compute) cluster security groups."
 }
 
-variable "vpc_forward_dns_zone" {
-  type        = string
+variable "using_packer_image" {
+  type        = bool
   nullable    = true
-  description = "GCP Cloud DNS zone name to be used for scale cluster (Ex: example-zone)."
+  description = "If true, gpfs rpm copy step will be skipped during the configuration."
 }
 
-variable "vpc_reverse_dns_zone" {
+variable "using_rest_api_remote_mount" {
   type        = string
   nullable    = true
-  description = "GCP Cloud DNS reverse zone lookup to be used for scale cluster (Ex: example-zone-reverse)."
+  description = "If false, skips GUI initialization on compute cluster for remote mount configuration."
 }
+
+
+
+
+
+
+
+
+
+
+
+
+variable "vpc_availability_zones" {
+  type        = list(string)
+  nullable    = false
+  description = "A list of availability zones names or ids in the region."
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 # Note:
 # 1. A private DNS Zone name will be created "resource_prefix" to store A/forward records
@@ -353,32 +383,56 @@ variable "vpc_compute_cluster_dns_domain" { # equivalent to DNS name
   description = "GCP Cloud DNS domain name to be used for compute cluster."
 }
 
+variable "vpc_compute_cluster_private_subnets" {
+  type        = list(string)
+  nullable    = true
+  description = "List of IDs of compute cluster private subnets."
+}
+
+variable "vpc_compute_cluster_private_subnets_cidr_block" {
+  type        = string
+  nullable    = true
+  description = "cidr_block of compute private subnet."
+}
+
+variable "vpc_forward_dns_zone" {
+  type        = string
+  nullable    = true
+  description = "GCP Cloud DNS zone name to be used for scale cluster (Ex: example-zone)."
+}
+
+variable "vpc_ref" {
+  type        = string
+  nullable    = false
+  description = "VPC id were to deploy the bastion."
+}
+
+variable "vpc_region" {
+  type        = string
+  nullable    = false
+  description = "GCP region where the resources will be created."
+}
+
+variable "vpc_reverse_dns_zone" {
+  type        = string
+  nullable    = true
+  description = "GCP Cloud DNS reverse zone lookup to be used for scale cluster (Ex: example-zone-reverse)."
+}
+
 variable "vpc_storage_cluster_dns_domain" { # equivalent to DNS name
   type        = string
   nullable    = true
   description = "GCP Cloud DNS domain name to be used for storage cluster."
 }
 
-variable "gateway_instance_type" {
+variable "vpc_storage_cluster_private_subnets" {
+  type        = list(string)
+  nullable    = true
+  description = "List of IDs of storage cluster private subnets."
+}
+
+variable "vpc_storage_cluster_private_subnets_cidr_block" {
   type        = string
   nullable    = true
-  description = "Instance type to use for provisioning the gateway instances."
-}
-
-variable "total_gateway_instances" {
-  type        = number
-  nullable    = true
-  description = "Number of EC2 instances to be launched for gateway nodes."
-}
-
-variable "protocol_instance_type" {
-  type        = string
-  nullable    = true
-  description = "Instance type to use for provisioning the protocol instances."
-}
-
-variable "total_protocol_instances" {
-  type        = number
-  nullable    = true
-  description = "Number of EC2 instances to be launched for protocol nodes."
+  description = "cidr_block of storage private subnet."
 }
