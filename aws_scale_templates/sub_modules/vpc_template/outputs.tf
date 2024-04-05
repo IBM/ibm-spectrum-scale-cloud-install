c94c7c61065ac1e8efdf230c8b93463cc0ecbd78
Sasikanth Eda <sasikeda@users.noreply.github.com>
2024-04-05 15:39:34 +0530

    Rearrange outputs to ascending order (#501)
    Signed-off-by: Sasikanth Eda <sasikanth.eda@in.ibm.com>

--- aws_scale_templates/sub_modules/vpc_template/outputs.tf
+++ aws_scale_templates/sub_modules/vpc_template/outputs.tf
@@ -1,26 +1,6 @@
-output "vpc_ref" {
-  value       = module.vpc.vpc_id
-  description = "The ID of the VPC."
-}
-
-output "vpc_public_subnets" {
-  value       = module.public_subnet.subnet_id
-  description = "List of IDs of public subnets."
-}
-
-output "vpc_storage_cluster_private_subnets" {
-  value       = module.storage_private_subnet.subnet_id
-  description = "List of IDs of storage cluster private subnets."
-}
-
 output "vpc_compute_cluster_private_subnets" {
   value       = module.compute_private_subnet.subnet_id
   description = "List of IDs of compute cluster private subnets."
-}
-
-output "vpc_storage_nat_gateways" {
-  value       = module.storage_nat_gateway.nat_gw_id
-  description = "List of allocation ID of Elastic IPs created for AWS NAT Gateway."
 }
 
 output "vpc_compute_nat_gateways" {
@@ -33,12 +13,32 @@ output "vpc_internet_gateway" {
   description = "The ID of the Internet Gateway."
 }
 
-output "vpc_s3_public_endpoint" {
-  value       = module.vpc_public_endpoint.vpce_id
-  description = "The ID of the vpc s3 endpoint associated with public subnets."
+output "vpc_public_subnets" {
+  value       = module.public_subnet.subnet_id
+  description = "List of IDs of public subnets."
+}
+
+output "vpc_ref" {
+  value       = module.vpc.vpc_id
+  description = "The ID of the VPC."
 }
 
 output "vpc_s3_private_endpoint" {
   value       = module.vpc_private_endpoint.vpce_id
   description = "The ID of the vpc s3 endpoint associated with private subnets."
+}
+
+output "vpc_s3_public_endpoint" {
+  value       = module.vpc_public_endpoint.vpce_id
+  description = "The ID of the vpc s3 endpoint associated with public subnets."
+}
+
+output "vpc_storage_cluster_private_subnets" {
+  value       = module.storage_private_subnet.subnet_id
+  description = "List of IDs of storage cluster private subnets."
+}
+
+output "vpc_storage_nat_gateways" {
+  value       = module.storage_nat_gateway.nat_gw_id
+  description = "List of allocation ID of Elastic IPs created for AWS NAT Gateway."
 }
