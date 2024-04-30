ad8a1b18d00c681d1a49258a68f73459cf8f4d89
Sasikanth Eda <sasikanth.eda@in.ibm.com>
2024-04-30 14:20:19 +0530

    Azure - packer improvements (root resize, private mode)
    Signed-off-by: Sasikanth Eda <sasikanth.eda@in.ibm.com>

--- azure_scale_templates/sub_modules/vpc_template/variables.tf
+++ azure_scale_templates/sub_modules/vpc_template/variables.tf
@@ -1,21 +1,77 @@
 variable "client_id" {
   type        = string
+  nullable    = false
+  sensitive   = true
   description = "The Active Directory service principal associated with your account."
 }
 
 variable "client_secret" {
   type        = string
+  nullable    = false
+  sensitive   = true
   description = "The password or secret for your service principal."
 }
 
-variable "tenant_id" {
+variable "cluster_type" {
   type        = string
-  description = "The Active Directory tenant identifier, must provide when using service principals."
+  nullable    = false
+  description = "Cluster type to provision. Examples: Storage-only, Compute-only, Combined-compute-storage."
+}
+
+variable "create_resouce_group" {
+  type        = bool
+  nullable    = true
+  description = "Creates resouce group."
+}
+
+variable "resource_group_name" {
+  type        = string
+  nullable    = true
+  description = "The name of a new resource group in which the resources will be created."
+}
+
+variable "resource_prefix" {
+  type        = string
+  nullable    = false
+  description = "Prefix is added to all resources that are created. Example: ibm-storage-scale"
 }
 
 variable "subscription_id" {
   type        = string
+  nullable    = false
+  sensitive   = true
   description = "The subscription ID to use."
+}
+
+variable "tenant_id" {
+  type        = string
+  nullable    = false
+  sensitive   = true
+  description = "The Active Directory tenant identifier, must provide when using service principals."
+}
+
+variable "vpc_bastion_service_subnets_cidr_blocks" {
+  type        = list(string)
+  nullable    = true
+  description = "List of CIDR blocks for azure fully managed bastion subnet."
+}
+
+variable "vpc_cidr_block" {
+  type        = string
+  nullable    = false
+  description = "The CIDR block for the vpc."
+}
+
+variable "vpc_compute_cluster_private_subnets_cidr_blocks" {
+  type        = list(string)
+  nullable    = true
+  description = "List of cidr_blocks for compute cluster private subnets."
+}
+
+variable "vpc_public_subnets_cidr_blocks" {
+  type        = list(string)
+  nullable    = true
+  description = "List of cidr_blocks of public subnets."
 }
 
 variable "vpc_region" {
@@ -24,63 +80,14 @@ variable "vpc_region" {
   description = "The location/region of the vpc to create. Examples are East US, West US, etc."
 }
 
-variable "resource_prefix" {
-  type        = string
-  nullable    = true
-  default     = "ibm-storage-scale"
-  description = "Prefix is added to all resources that are created."
-}
-
-variable "resource_group_name" {
-  type        = string
-  description = "The name of a new resource group in which the resources will be created."
-}
-
-variable "vpc_cidr_block" {
-  type        = string
-  nullable    = true
-  default     = null
-  description = "The CIDR block for the vpc."
-}
-
-variable "vpc_public_subnets_cidr_blocks" {
-  type        = list(string)
-  nullable    = true
-  default     = null
-  description = "List of cidr_blocks of public subnets."
-}
-
 variable "vpc_storage_cluster_private_subnets_cidr_blocks" {
   type        = list(string)
   nullable    = true
-  default     = null
   description = "List of cidr_blocks for storage cluster private subnets."
-}
-
-variable "vpc_compute_cluster_private_subnets_cidr_blocks" {
-  type        = list(string)
-  nullable    = true
-  default     = null
-  description = "List of cidr_blocks for compute cluster private subnets."
 }
 
 variable "vpc_tags" {
   type        = map(string)
   nullable    = true
-  default     = {}
   description = "The tags to associate with your network and subnets."
-}
-
-variable "vpc_bastion_service_subnets_cidr_blocks" {
-  type        = list(string)
-  nullable    = true
-  default     = null
-  description = "List of cidr_blocks for azure fully managed subnet."
-}
-
-variable "create_resouce_group" {
-  type        = bool
-  nullable    = true
-  default     = null
-  description = "Creates resouce group."
 }
---
+++ packer_templates/azure/install.sh
@@ -0,0 +1,125 @@
+#!/bin/bash
+set -ex
+
+sleep 30
+if [ -f /etc/os-release ] && grep -qiE 'Ubuntu' /etc/os-release; then
+    sudo rm -rf /var/log/ubuntu-advantage.log
+    sudo cloud-init clean --machine-id
+elif [ -f /etc/os-release ] && grep -qiE 'redhat' /etc/os-release; then
+    # Resize the root from default 2Gi
+    sudo lvextend -r -l +100%FREE /dev/mapper/rootvg-rootlv
+    sudo dnf install -y unzip python3 python3-pip jq numactl
+    sudo dnf install -y kernel-devel-`uname -r` kernel-headers-`uname -r`
+    sudo dnf install -y make gcc-c++ elfutils-libelf-devel bind-utils nftables iptables nvme-cli
+    sudo sh -c "echo '[IBMScaleRepository]' >> /etc/yum.repos.d/scale.repo"
+    sudo sh -c "echo 'name=IBM Storage Scale Repository' >> /etc/yum.repos.d/scale.repo"
+    sudo sh -c "echo 'baseurl=$STORAGE_ACCOUNT_URL/$SCALE_VERSION/gpfs_rpms/' >> /etc/yum.repos.d/scale.repo"
+    sudo sh -c "echo 'enabled=1' >> /etc/yum.repos.d/scale.repo"
+    sudo sh -c "echo 'gpgcheck=1' >> /etc/yum.repos.d/scale.repo"
+    sudo sh -c "echo 'gpgkey=$STORAGE_ACCOUNT_URL/$SCALE_VERSION/Public_Keys/Storage_Scale_public_key.pgp' >> /etc/yum.repos.d/scale.repo"
+    sudo sh -c "echo -e '\n' >> /etc/yum.repos.d/scale.repo"
+    sudo sh -c "echo '[ZimonRepository]' >> /etc/yum.repos.d/scale.repo"
+    sudo sh -c "echo 'name=IBM Storage Scale Zimon Repository' >> /etc/yum.repos.d/scale.repo"
+    if sudo grep -q el8 /etc/os-release; then
+        sudo sh -c "echo 'baseurl=$STORAGE_ACCOUNT_URL/$SCALE_VERSION/zimon_rpms/rhel8/' >> /etc/yum.repos.d/scale.repo"
+    elif sudo grep -q el9 /etc/os-release; then
+        sudo sh -c "echo 'baseurl=$STORAGE_ACCOUNT_URL/$SCALE_VERSION/zimon_rpms/rhel9/' >> /etc/yum.repos.d/scale.repo"
+    fi
+    sudo sh -c "echo 'enabled=1' >> /etc/yum.repos.d/scale.repo"
+    sudo sh -c "echo 'gpgcheck=1' >> /etc/yum.repos.d/scale.repo"
+    sudo sh -c "echo 'gpgkey=$STORAGE_ACCOUNT_URL/$SCALE_VERSION/Public_Keys/Storage_Scale_public_key.pgp' >> /etc/yum.repos.d/scale.repo"
+    sudo sh -c "echo -e '\n' >> /etc/yum.repos.d/scale.repo"
+    sudo dnf install -y gpfs.base gpfs.docs gpfs.msg.en* gpfs.compression gpfs.ext gpfs.gpl gpfs.gskit gpfs.gui gpfs.java gpfs.gss.pmcollector gpfs.gss.pmsensors gpfs.afm.cos gpfs.compression gpfs.license*
+    if sudo dnf search gpfs.adv | grep -q "gpfs.adv"; then
+        sudo dnf install -y gpfs.adv
+    fi
+    if sudo dnf search gpfs.crypto | grep -q "gpfs.crypto"; then
+        sudo dnf install -y gpfs.crypto
+    fi
+
+    install_nfs() {
+        sudo sh -c "echo '[NFSProtocolRepository]' >> /etc/yum.repos.d/scale.repo"
+        sudo sh -c "echo 'name=IBM Storage Scale NFS Protocol Repository' >> /etc/yum.repos.d/scale.repo"
+        if sudo grep -q el8 /etc/os-release; then
+            sudo sh -c "echo 'baseurl=$STORAGE_ACCOUNT_URL/$SCALE_VERSION/ganesha_rpms/rhel8/' >> /etc/yum.repos.d/scale.repo"
+        elif sudo grep -q el9 /etc/os-release; then
+            sudo sh -c "echo 'baseurl=$STORAGE_ACCOUNT_URL/$SCALE_VERSION/ganesha_rpms/rhel9/' >> /etc/yum.repos.d/scale.repo"
+        fi
+        sudo sh -c "echo 'enabled=1' >> /etc/yum.repos.d/scale.repo"
+        sudo sh -c "echo 'gpgcheck=1' >> /etc/yum.repos.d/scale.repo"
+        sudo sh -c "echo 'gpgkey=$STORAGE_ACCOUNT_URL/$SCALE_VERSION/Public_Keys/Storage_Scale_public_key.pgp' >> /etc/yum.repos.d/scale.repo"
+        sudo sh -c "echo -e '\n' >> /etc/yum.repos.d/scale.repo"
+        sudo dnf install -y gpfs.nfs-ganesha gpfs.nfs-ganesha-gpfs gpfs.nfs-ganesha-utils
+        sudo dnf install -y gpfs.pm-ganesha
+    }
+
+    install_smb() {
+        sudo sh -c "echo '[SMBProtocolRepository]' >> /etc/yum.repos.d/scale.repo"
+        sudo sh -c "echo 'name=IBM Storage Scale SMB Protocol Repository' >> /etc/yum.repos.d/scale.repo"
+        if sudo grep -q el8 /etc/os-release; then
+            sudo sh -c "echo 'baseurl=$STORAGE_ACCOUNT_URL/$SCALE_VERSION/smb_rpms/rhel8/' >> /etc/yum.repos.d/scale.repo"
+        elif sudo grep -q el9 /etc/os-release; then
+            sudo sh -c "echo 'baseurl=$STORAGE_ACCOUNT_URL/$SCALE_VERSION/smb_rpms/rhel9/' >> /etc/yum.repos.d/scale.repo"
+        fi
+        sudo sh -c "echo 'enabled=1' >> /etc/yum.repos.d/scale.repo"
+        sudo sh -c "echo 'gpgcheck=1' >> /etc/yum.repos.d/scale.repo"
+        sudo sh -c "echo 'gpgkey=$STORAGE_ACCOUNT_URL/$SCALE_VERSION/Public_Keys/Storage_Scale_public_key.pgp' >> /etc/yum.repos.d/scale.repo"
+        sudo sh -c "echo -e '\n' >> /etc/yum.repos.d/scale.repo"
+        sudo dnf install -y gpfs.smb
+    }
+
+    install_s3() {
+        sudo sh -c "echo '[S3ProtocolRepository]' >> /etc/yum.repos.d/scale.repo"
+        sudo sh -c "echo 'name=IBM Storage Scale S3 Protocol Repository' >> /etc/yum.repos.d/scale.repo"
+        if sudo grep -q el8 /etc/os-release; then
+            sudo sh -c "echo 'baseurl=$STORAGE_ACCOUNT_URL/$SCALE_VERSION/s3_rpms/rhel8/' >> /etc/yum.repos.d/scale.repo"
+        elif sudo grep -q el9 /etc/os-release; then
+            sudo sh -c "echo 'baseurl=$STORAGE_ACCOUNT_URL/$SCALE_VERSION/s3_rpms/rhel9/' >> /etc/yum.repos.d/scale.repo"
+        fi
+        sudo sh -c "echo 'enabled=1' >> /etc/yum.repos.d/scale.repo"
+        sudo sh -c "echo 'gpgcheck=1' >> /etc/yum.repos.d/scale.repo"
+        sudo sh -c "echo 'gpgkey=$STORAGE_ACCOUNT_URL/$SCALE_VERSION/Public_Keys/Storage_Scale_public_key.pgp' >> /etc/yum.repos.d/scale.repo"
+        sudo sh -c "echo -e '\n' >> /etc/yum.repos.d/scale.repo"
+        sudo dnf install -y gpfs.mms3 noobaa-core
+    }
+
+    case "$INSTALL_PROTOCOLS" in
+        nfs)
+            install_nfs
+            ;;
+        smb)
+            install_smb
+            ;;
+        s3)
+            install_s3
+            ;;
+        nfs-s3)
+            install_nfs
+            install_s3
+            ;;
+        nfs-smb)
+            install_nfs
+            install_smb
+            ;;
+        smb-s3)
+            install_smb
+            install_s3
+            ;;
+        *)
+            install_nfs
+            install_smb
+            install_s3
+            ;;
+    esac
+
+    sudo /usr/lpp/mmfs/bin/mmbuildgpl
+    sudo sh -c "echo 'export PATH=$PATH:$HOME/bin:/usr/lpp/mmfs/bin' >> /root/.bashrc"
+    sudo rm -rf /etc/yum.repos.d/scale.repo
+    sudo dnf clean all
+    sudo rm -rf /var/cache/dnf
+    sudo rm -rf /root/.ssh/authorized_keys
+    sudo rm -rf /home/$SSH_USERNAME/authorized_keys
+    sudo rm -rf /root/.bash_history
+    sudo rm -rf /home/$SSH_USERNAME/.bash_history
+    sudo /usr/sbin/waagent -force -deprovision+user && export HISTSIZE=0 && sync
+fi
