f6b3fe301c8feba5c7eee92ab2c266d3349ba90d
Praveen Ramu <118440436+praveenramuu@users.noreply.github.com>
2024-05-05 11:23:07 +0530

    AWS Adding PTR record for CES nodes (#514)
    * Adding PTR record for CES nodes
    
    Signed-off-by: Praveen Ramu <praveen.ramu@ibm.com>
    
    * Append dns domain for CES PTR record
    
    Signed-off-by: Praveen Ramu <praveen.ramu@ibm.com>
    
    * secondary_private_ip check for CES PTR
    
    Signed-off-by: Praveen Ramu <praveen.ramu@ibm.com>
    
    ---------
    
    Signed-off-by: Praveen Ramu <praveen.ramu@ibm.com>

--- resources/aws/compute/ec2_0_vol/ec2_0_vol.tf
+++ resources/aws/compute/ec2_0_vol/ec2_0_vol.tf
@@ -125,6 +125,16 @@ resource "aws_route53_record" "ces_a_itself" {
   ttl     = 360
 }
 
+# Create "PTR" (Pointer) to enables reverse DNS lookup, from an IP address to a hostname for CES ip address
+resource "aws_route53_record" "ces_ptr_itself" {
+  count   = var.secondary_private_ip != null ? 1 : 0
+  zone_id = var.reverse_dns_zone
+  type    = "PTR"
+  name    = format("%s.%s.%s.%s", split(".", var.secondary_private_ip)[3], split(".", var.secondary_private_ip)[2], split(".", var.secondary_private_ip)[1], var.reverse_dns_domain)
+  records = [format("%s-ces.%s", var.name_prefix, var.dns_domain)]
+  ttl     = 360
+}
+
 output "instance_details" {
   value = {
     private_ip = aws_instance.itself.private_ip
