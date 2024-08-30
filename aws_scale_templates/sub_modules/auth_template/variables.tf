9a3ba5a0ea413cdc625842d55de8e98e440c53d2
Sasikanth Eda <sasikeda@users.noreply.github.com>
2024-08-30 09:23:44 +0530

    Add ldap vm resource (#565)
    * Add ldap vm resource
    
    Signed-off-by: Sasikanth Eda <sasikanth.eda@in.ibm.com>
    
    * Initialize with defaults to null
    
    Signed-off-by: Sasikanth Eda <sasikanth.eda@in.ibm.com>
    
    * Add optional turn-on
    
    Signed-off-by: Sasikanth Eda <sasikanth.eda@in.ibm.com>
    
    * Fix the output condition
    
    Signed-off-by: Sasikanth Eda <sasikanth.eda@in.ibm.com>
    
    * Fix the typo
    
    Signed-off-by: Sasikanth Eda <sasikanth.eda@in.ibm.com>
    
    ---------
    
    Signed-off-by: Sasikanth Eda <sasikanth.eda@in.ibm.com>

--- aws_scale_templates/sub_modules/auth_template/variables.tf
+++ aws_scale_templates/sub_modules/auth_template/variables.tf
@@ -4,14 +4,58 @@ variable "create_cloud_managed_auth" {
   description = "Flag to represent if a cloud-managed auth service needs to be created or customer managed auth service needs to be created."
 }
 
+variable "ldap_image_ref" {
+  type        = string
+  default     = null
+  nullable    = true
+  description = "ID of AMI to use for provisioning the ldap instance."
+}
+
+variable "ldap_instance_boot_disk_type" {
+  type        = string
+  default     = null
+  nullable    = true
+  description = "EBS volume types: standard, gp2, gp3, io1, io2 and sc1 or st1."
+}
+
+variable "ldap_instance_key_pair" {
+  type        = string
+  default     = null
+  nullable    = true
+  description = "The key pair to use to launch the ldap instance."
+}
+
+variable "ldap_instance_private_subnet" {
+  type        = string
+  default     = null
+  nullable    = true
+  description = "OpenLDAP private subnet."
+}
+
+variable "ldap_instance_type" {
+  type        = string
+  default     = null
+  nullable    = true
+  description = "Instance type to use for provisioning the ldap instance."
+}
+
+variable "ldap_public_ssh_port" {
+  type        = number
+  default     = null
+  nullable    = true
+  description = "Set the SSH port to use from desktop to the ldap."
+}
+
 variable "managed_ad_dns_name" {
   type        = string
+  default     = null
   nullable    = true
   description = "Managed directory DNS name"
 }
 
 variable "managed_ad_password" {
   type        = string
+  default     = null
   nullable    = true
   sensitive   = true
   description = "Managed directory (AD) password"
@@ -19,24 +63,68 @@ variable "managed_ad_password" {
 
 variable "managed_ad_size" {
   type        = string
+  default     = null
   nullable    = true
   description = "Managed directory (AD) size"
 }
 
 variable "managed_ad_subnet_refs" {
   type        = list(string)
+  default     = []
   nullable    = true
-  description = "Managed directory (AD) subnets ()."
+  description = "Managed directory (AD) subnets (they must belong to two different availability zones)."
+}
+
+variable "remote_cidr_blocks" {
+  type        = list(string)
+  default     = []
+  nullable    = true
+  description = "List of CIDRs that can access to the ldap. Example: 0.0.0.0/0"
+}
+
+variable "resource_prefix" {
+  type        = string
+  default     = null
+  nullable    = true
+  description = "Prefix is added to all resources that are created."
+}
+
+variable "vpc_dns_domain" {
+  type        = string
+  default     = null
+  nullable    = true
+  description = "DNS domain name to be used for ldap instance."
+}
+
+variable "vpc_forward_dns_zone" {
+  type        = string
+  default     = null
+  nullable    = true
+  description = "DNS zone name to be used for scale cluster (Ex: example-zone)."
 }
 
 variable "vpc_ref" {
   type        = string
   nullable    = false
-  description = "VPC id to be associated with the DNS zone."
+  description = "VPC id to be associated with the AD zone."
 }
 
 variable "vpc_region" {
   type        = string
   nullable    = false
   description = "The region where AWS operations will take place. Examples are us-east-1, us-west-2, etc."
+}
+
+variable "vpc_reverse_dns_domain" {
+  type        = string
+  default     = null
+  nullable    = true
+  description = "DNS reverse domain (Ex: 10.in-addr.arpa)."
+}
+
+variable "vpc_reverse_dns_zone" {
+  type        = string
+  default     = null
+  nullable    = true
+  description = "DNS reverse zone lookup to be used for scale cluster (Ex: example-zone-reverse)."
 }
---
+++ resources/aws/compute/ec2_ldap/ec2_ldap.tf
@@ -0,0 +1,108 @@
+/*
+     Creates AWS EC2 OpenLDAP instance.
+*/
+
+variable "ami_id" {}
+variable "dns_domain" {}
+variable "forward_dns_zone" {}
+variable "iam_instance_profile" {}
+variable "instance_type" {}
+variable "name_prefix" {}
+variable "reverse_dns_domain" {}
+variable "reverse_dns_zone" {}
+variable "root_volume_type" {}
+variable "security_groups" {}
+variable "subnet_id" {}
+variable "turn_on" {}
+variable "user_public_key" {}
+
+data "template_file" "user_data" {
+  count    = var.turn_on ? 1 : 0
+  template = <<EOF
+#!/usr/bin/env bash
+# Hostname settings
+hostnamectl set-hostname --static "${var.name_prefix}.${var.dns_domain}"
+echo 'preserve_hostname: True' > /etc/cloud/cloud.cfg.d/10_hostname.cfg
+echo "${var.name_prefix}.${var.dns_domain}" > /etc/hostname
+EOF
+}
+
+data "template_cloudinit_config" "user_data64" {
+  count         = var.turn_on ? 1 : 0
+  gzip          = true
+  base64_encode = true
+  part {
+    content_type = "text/x-shellscript"
+    content      = try(data.template_file.user_data[0].rendered, null)
+  }
+}
+
+resource "aws_instance" "itself" {
+  count           = var.turn_on ? 1 : 0
+  ami             = var.ami_id
+  instance_type   = var.instance_type
+  key_name        = var.user_public_key
+  security_groups = var.security_groups
+  subnet_id       = var.subnet_id
+
+  # Only include iam_instance_profile if var.iam_instance_profile is a non-empty string
+  # otherwise, skip the parameter entirely
+  iam_instance_profile = var.iam_instance_profile != "" ? var.iam_instance_profile : null
+
+  root_block_device {
+    encrypted             = true # TODO: Custom key
+    volume_type           = var.root_volume_type
+    delete_on_termination = true
+  }
+
+  volume_tags = merge(
+    {
+      "Name" = format("%s-root", var.name_prefix)
+    },
+  )
+
+  user_data_base64 = try(data.template_cloudinit_config.user_data64[0].rendered, null)
+  tags = merge(
+    {
+      "Name" = var.name_prefix
+    },
+  )
+
+  metadata_options {
+    http_endpoint = "enabled"
+    http_tokens   = "required"
+  }
+
+  lifecycle {
+    ignore_changes = all
+  }
+}
+
+# Create "A" (IPv4 Address) record to map IPv4 address as hostname along with domain
+resource "aws_route53_record" "a_itself" {
+  count   = var.turn_on ? 1 : 0
+  zone_id = var.forward_dns_zone
+  type    = "A"
+  name    = var.name_prefix
+  records = [aws_instance.itself[0].private_ip]
+  ttl     = 360
+}
+
+# Create "PTR" (Pointer) to enable reverse DNS lookup, from an IP address to a hostname
+resource "aws_route53_record" "ptr_itself" {
+  count   = var.turn_on ? 1 : 0
+  zone_id = var.reverse_dns_zone
+  type    = "PTR"
+  name    = format("%s.%s.%s.%s", split(".", aws_instance.itself[0].private_ip)[3], split(".", aws_instance.itself[0].private_ip)[2], split(".", aws_instance.itself[0].private_ip)[1], var.reverse_dns_domain)
+  records = [format("%s.%s", var.name_prefix, var.dns_domain)]
+  ttl     = 360
+}
+
+output "instance_details" {
+  value = length(aws_instance.itself) != 0 ? {
+    private_ip = aws_instance.itself[0].private_ip
+    id         = aws_instance.itself[0].id
+    dns        = format("%s.%s", var.name_prefix, var.dns_domain)
+    zone       = aws_instance.itself[0].availability_zone
+  } : {}
+}
