fe408e033a7876fdf42584c4524148e560786cc3
Sasikanth Eda <sasikeda@users.noreply.github.com>
2024-09-02 10:44:11 +0530

    [AWS] Add ubuntu image support (#566)
    * Add ubuntu22 support to aws packer
    
    Signed-off-by: Sasikanth Eda <sasikanth.eda@in.ibm.com>
    
    * Use apt-get instead of apt
    
    Signed-off-by: Sasikanth Eda <sasikanth.eda@in.ibm.com>
    
    * Update ubuntu to use protocol debs
    
    Signed-off-by: Sasikanth Eda <sasikanth.eda@in.ibm.com>
    
    * Add the mmbuildgpl dependency
    
    Signed-off-by: Sasikanth Eda <sasikanth.eda@in.ibm.com>
    
    ---------
    
    Signed-off-by: Sasikanth Eda <sasikanth.eda@in.ibm.com>

--- packer_templates/aws/install.sh
+++ packer_templates/aws/install.sh
@@ -3,15 +3,26 @@ set -ex
 
 sleep 30
 if [ -f /etc/os-release ] && grep -qiE 'Ubuntu' /etc/os-release; then
-    sudo apt install unzip
+    echo "debconf debconf/frontend select Noninteractive" | sudo debconf-set-selections
+    sudo apt-get -y install make cpp gcc g++
+    sudo apt-get -y install unzip
     sudo curl https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip -o awscliv2.zip
     sudo unzip awscliv2.zip
     sudo ./aws/install
     sudo rm -rf awscliv2.zip
     curl -sS http://$PACKAGE_REPOSITORY.s3-website.$VPC_REGION.amazonaws.com/$SCALE_VERSION/Public_Keys/Storage_Scale_public_key.pgp | gpg --dearmor | sudo tee /etc/apt/trusted.gpg.d/scale.gpg
-    sudo ua detach --assume-yes
-    sudo rm -rf /var/log/ubuntu-advantage.log
-    sudo cloud-init clean --machine-id
+    sudo sh -c "echo 'deb [trusted=yes] http://$PACKAGE_REPOSITORY.s3-website.$VPC_REGION.amazonaws.com/$SCALE_VERSION/gpfs_debs /' >> /etc/apt/sources.list.d/scale.list"
+    if sudo grep -q jammy /etc/os-release; then
+        sudo sh -c "echo 'deb [trusted=yes] http://$PACKAGE_REPOSITORY.s3-website.$VPC_REGION.amazonaws.com/$SCALE_VERSION/zimon_debs/ubuntu/ubuntu22 /' >> /etc/apt/sources.list.d/scale.list"
+    fi
+    sudo apt-get update
+    sudo apt-get -y install gpfs.base gpfs.docs gpfs.msg.en-us gpfs.compression gpfs.gpl gpfs.gskit gpfs.gui gpfs.java gpfs.afm.cos gpfs.license* gpfs.gss.pmcollector gpfs.gss.pmsensors
+    if sudo apt-cache search gpfs.adv | grep -q "gpfs.adv"; then
+        sudo apt-get -y install gpfs.adv
+    fi
+    if sudo apt-cache search gpfs.crypto | grep -q "gpfs.crypto"; then
+        sudo apt-get -y install gpfs.crypto
+    fi
 elif [ -f /etc/os-release ] && grep -qiE 'redhat' /etc/os-release; then
     sudo dnf install -y unzip python3 python3-pip jq numactl
     sudo dnf install -y kernel-devel-`uname -r` kernel-headers-`uname -r`
@@ -45,12 +56,20 @@ elif [ -f /etc/os-release ] && grep -qiE 'redhat' /etc/os-release; then
     if sudo dnf search gpfs.crypto | grep -q "gpfs.crypto"; then
         sudo dnf install -y gpfs.crypto
     fi
+fi
 
-    ces_failback() {
-        sudo cp /usr/lpp/mmfs/samples/cloud/ces_middleware/mmcesExtendedIpMgmt.aws /var/mmfs/etc/mmcesExtendedIpMgmt
-    }
+ces_failback() {
+    sudo cp /usr/lpp/mmfs/samples/cloud/ces_middleware/mmcesExtendedIpMgmt.aws /var/mmfs/etc/mmcesExtendedIpMgmt
+}
 
-    install_nfs() {
+install_nfs() {
+    if [ -f /etc/os-release ] && grep -qiE 'Ubuntu' /etc/os-release; then
+        if sudo grep -q jammy /etc/os-release; then
+            sudo sh -c "echo 'deb [trusted=yes] http://$PACKAGE_REPOSITORY.s3-website.$VPC_REGION.amazonaws.com/$SCALE_VERSION/ganesha_debs/ubuntu/ubuntu22 /' >> /etc/apt/sources.list.d/scale.list"
+        fi
+        sudo apt-get update
+        sudo apt-get -y install gpfs.nfs-ganesha gpfs.nfs-ganesha-gpfs gpfs.nfs-ganesha-utils
+    elif [ -f /etc/os-release ] && grep -qiE 'redhat' /etc/os-release; then
         sudo sh -c "echo '[NFSProtocolRepository]' >> /etc/yum.repos.d/scale.repo"
         sudo sh -c "echo 'name=IBM Storage Scale NFS Protocol Repository' >> /etc/yum.repos.d/scale.repo"
         if sudo grep -q el8 /etc/os-release; then
@@ -64,9 +83,17 @@ elif [ -f /etc/os-release ] && grep -qiE 'redhat' /etc/os-release; then
         sudo sh -c "echo -e '\n' >> /etc/yum.repos.d/scale.repo"
         sudo dnf install -y gpfs.nfs-ganesha gpfs.nfs-ganesha-gpfs gpfs.nfs-ganesha-utils
         sudo dnf install -y gpfs.pm-ganesha
-    }
+    fi
+}
 
-    install_smb() {
+install_smb() {
+    if [ -f /etc/os-release ] && grep -qiE 'Ubuntu' /etc/os-release; then
+        if sudo grep -q jammy /etc/os-release; then
+            sudo sh -c "echo 'deb [trusted=yes] http://$PACKAGE_REPOSITORY.s3-website.$VPC_REGION.amazonaws.com/$SCALE_VERSION/smb_debs/ubuntu/ubuntu22 /' >> /etc/apt/sources.list.d/scale.list"
+        fi
+        sudo apt-get update
+        sudo apt-get -y install gpfs.nfs-ganesha gpfs.nfs-ganesha-gpfs gpfs.nfs-ganesha-utils
+    elif [ -f /etc/os-release ] && grep -qiE 'redhat' /etc/os-release; then
         sudo sh -c "echo '[SMBProtocolRepository]' >> /etc/yum.repos.d/scale.repo"
         sudo sh -c "echo 'name=IBM Storage Scale SMB Protocol Repository' >> /etc/yum.repos.d/scale.repo"
         if sudo grep -q el8 /etc/os-release; then
@@ -79,9 +106,11 @@ elif [ -f /etc/os-release ] && grep -qiE 'redhat' /etc/os-release; then
         sudo sh -c "echo 'gpgkey=http://$PACKAGE_REPOSITORY.s3-website.$VPC_REGION.amazonaws.com/$SCALE_VERSION/Public_Keys/Storage_Scale_public_key.pgp' >> /etc/yum.repos.d/scale.repo"
         sudo sh -c "echo -e '\n' >> /etc/yum.repos.d/scale.repo"
         sudo dnf install -y gpfs.smb
-    }
+    fi
+}
 
-    install_s3() {
+install_s3() {
+    if [ -f /etc/os-release ] && grep -qiE 'redhat' /etc/os-release; then
         sudo sh -c "echo '[S3ProtocolRepository]' >> /etc/yum.repos.d/scale.repo"
         sudo sh -c "echo 'name=IBM Storage Scale S3 Protocol Repository' >> /etc/yum.repos.d/scale.repo"
         if sudo grep -q el8 /etc/os-release; then
@@ -94,49 +123,57 @@ elif [ -f /etc/os-release ] && grep -qiE 'redhat' /etc/os-release; then
         sudo sh -c "echo 'gpgkey=http://$PACKAGE_REPOSITORY.s3-website.$VPC_REGION.amazonaws.com/$SCALE_VERSION/Public_Keys/Storage_Scale_public_key.pgp' >> /etc/yum.repos.d/scale.repo"
         sudo sh -c "echo -e '\n' >> /etc/yum.repos.d/scale.repo"
         sudo dnf install -y gpfs.mms3 noobaa-core
-    }
+    fi
+}
 
-    case "$INSTALL_PROTOCOLS" in
-        None)
-            echo "skipping protocol rpm installation"
-            ;;
-        nfs)
-            ces_failback
-            install_nfs
-            ;;
-        smb)
-            ces_failback
-            install_smb
-            ;;
-        s3)
-            ces_failback
-            install_s3
-            ;;
-        nfs-s3)
-            ces_failback
-            install_nfs
-            install_s3
-            ;;
-        nfs-smb)
-            ces_failback
-            install_nfs
-            install_smb
-            ;;
-        smb-s3)
-            ces_failback
-            install_smb
-            install_s3
-            ;;
-        *)
-            ces_failback
-            install_nfs
-            install_smb
-            install_s3
-            ;;
-    esac
+case "$INSTALL_PROTOCOLS" in
+    None)
+        echo "skipping protocol rpm/debs installation"
+        ;;
+    nfs)
+        ces_failback
+        install_nfs
+        ;;
+    smb)
+        ces_failback
+        install_smb
+        ;;
+    s3)
+        ces_failback
+        install_s3
+        ;;
+    nfs-s3)
+        ces_failback
+        install_nfs
+        install_s3
+        ;;
+    nfs-smb)
+        ces_failback
+        install_nfs
+        install_smb
+        ;;
+    smb-s3)
+        ces_failback
+        install_smb
+        install_s3
+        ;;
+    *)
+        ces_failback
+        install_nfs
+        install_smb
+        install_s3
+        ;;
+esac
 
-    sudo /usr/lpp/mmfs/bin/mmbuildgpl
-    sudo sh -c "echo 'export PATH=$PATH:$HOME/bin:/usr/lpp/mmfs/bin' >> /root/.bashrc"
+sudo /usr/lpp/mmfs/bin/mmbuildgpl
+sudo sh -c "echo 'export PATH=$PATH:$HOME/bin:/usr/lpp/mmfs/bin' >> /root/.bashrc"
+if [ -f /etc/os-release ] && grep -qiE 'Ubuntu' /etc/os-release; then
+    sudo rm -rf /etc/apt/sources.list.d/scale.list
+    sudo apt-get clean
+    sudo ua detach --assume-yes
+    sudo rm -rf /var/log/ubuntu-advantage.log
+    sudo cloud-init clean --machine-id
+elif [ -f /etc/os-release ] && grep -qiE 'redhat' /etc/os-release; then
     sudo rm -rf /etc/yum.repos.d/scale.repo
     sudo dnf clean all
     sudo rm -rf /var/cache/dnf
