6f75833cfab51efe042ca1f319126846b410bf28
Jayesh-Kumar3 <Jayesh.Kumar3@ibm.com>
2024-11-27 17:31:51 +0530

    Removed vpc region var
    Signed-off-by: Jayesh-Kumar3 <Jayesh.Kumar3@ibm.com>

--- ibmcloud_scale_templates/sub_modules/instance_template/main.tf
+++ ibmcloud_scale_templates/sub_modules/instance_template/main.tf
@@ -1020,7 +1020,6 @@ module "storage_cluster_configuration" {
   clone_complete                      = module.prepare_ansible_configuration.clone_complete
   bastion_user                        = jsonencode(var.bastion_user)
   write_inventory_complete            = module.write_storage_cluster_inventory.write_inventory_complete
-  vpc_region                          = var.vpc_region
   inventory_format                    = var.inventory_format
   create_scale_cluster                = var.create_scale_cluster
   clone_path                          = var.scale_ansible_repo_clone_path
