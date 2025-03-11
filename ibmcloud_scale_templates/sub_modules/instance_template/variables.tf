f3104ad68ab746f8ca00cc88e28eee4d799c53f4
Jayesh-Kumar3 <Jayesh.Kumar3@ibm.com>
2025-03-11 23:10:43 +0530

    Added tie breaker profile variable
    Signed-off-by: Jayesh-Kumar3 <Jayesh.Kumar3@ibm.com>

--- ibmcloud_scale_templates/sub_modules/instance_template/variables.tf
+++ ibmcloud_scale_templates/sub_modules/instance_template/variables.tf
@@ -276,6 +276,12 @@ variable "storage_bare_metal_server_profile" {
   description = "Specify the virtual server instance profile type name to be used to create the Baremetal Storage nodes. For more information, see [Instance Profiles](https://cloud.ibm.com/docs/vpc?topic=vpc-bare-metal-servers-profile&interface=ui)."
 }
 
+variable "tie_breaker_bare_metal_server_profile" {
+  type        = string
+  default     = null
+  description = "Specify the virtual server instance profile type name to be used to create the Baremetal tie breaker nodes. For more information, see [Instance Profiles](https://cloud.ibm.com/docs/vpc?topic=vpc-bare-metal-servers-profile&interface=ui)."
+}
+
 variable "storage_bare_metal_osimage_name" {
   type        = string
   default     = "ibm-redhat-8-3-minimal-amd64-3"
